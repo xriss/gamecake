92471d07588fb6a6ae71462449bc1941554da9ca
kriss <kriss@localhost>
2011-07-24 18:33:25 +0100

    a basic android test build
    requires the following sdks to be installed into the sdks directory
    
    sdks/android-ndk
    sdks/android-sdk
    
    I think we can work it like this, ant can beused to build the java crap and I can setup premake to build the real code

---
+++ android/obj/local/armeabi/objs/sanangeles/demo.o.d
@@ -0,0 +1,107 @@
+/home/kriss/sdks/android-ndk/samples/sans/obj/local/armeabi/objs/sanangeles/demo.o: \
+ /home/kriss/sdks/android-ndk/samples/sans/jni/demo.c \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/stdlib.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/sys/cdefs.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/string.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/malloc.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/alloca.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/strings.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/sys/types.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/stdint.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/sys/_types.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/machine/_types.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/linux/posix_types.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/linux/stddef.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/linux/compiler.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/asm/posix_types.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/asm/types.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/linux/types.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/machine/kernel.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/memory.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/math.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/limits.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/sys/limits.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/linux/limits.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/machine/internal_types.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/machine/limits.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/sys/syslimits.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/assert.h \
+ /home/kriss/sdks/android-ndk/samples/sans/jni/importgl.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/GLES/gl.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/GLES/glplatform.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/KHR/khrplatform.h \
+ /home/kriss/sdks/android-ndk/samples/sans/jni/app.h \
+ /home/kriss/sdks/android-ndk/samples/sans/jni/shapes.h \
+ /home/kriss/sdks/android-ndk/samples/sans/jni/cams.h
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/stdlib.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/sys/cdefs.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/string.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/malloc.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/alloca.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/strings.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/sys/types.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/stdint.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/sys/_types.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/machine/_types.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/linux/posix_types.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/linux/stddef.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/linux/compiler.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/asm/posix_types.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/asm/types.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/linux/types.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/machine/kernel.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/memory.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/math.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/limits.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/sys/limits.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/linux/limits.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/machine/internal_types.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/machine/limits.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/sys/syslimits.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/assert.h:
+
+/home/kriss/sdks/android-ndk/samples/sans/jni/importgl.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/GLES/gl.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/GLES/glplatform.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/KHR/khrplatform.h:
+
+/home/kriss/sdks/android-ndk/samples/sans/jni/app.h:
+
+/home/kriss/sdks/android-ndk/samples/sans/jni/shapes.h:
+
+/home/kriss/sdks/android-ndk/samples/sans/jni/cams.h:
---
+++ android/obj/local/armeabi/objs/sanangeles/importgl.o.d
@@ -0,0 +1,23 @@
+/home/kriss/sdks/android-ndk/samples/sans/obj/local/armeabi/objs/sanangeles/importgl.o: \
+ /home/kriss/sdks/android-ndk/samples/sans/jni/importgl.c \
+ /home/kriss/sdks/android-ndk/samples/sans/jni/importgl.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/GLES/gl.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/GLES/glplatform.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/KHR/khrplatform.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/stdint.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/sys/_types.h \
+ /home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/machine/_types.h
+
+/home/kriss/sdks/android-ndk/samples/sans/jni/importgl.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/GLES/gl.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/GLES/glplatform.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/KHR/khrplatform.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/stdint.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/sys/_types.h:
+
+/home/kriss/sdks/android-ndk/platforms/android-5/arch-arm/usr/include/machine/_types.h:
