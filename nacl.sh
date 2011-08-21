98c3a071ec5871cce00723bde7178ab61496c859
kriss <kriss@localhost>
2011-08-21 16:00:28 +0100

    moved sdks

--- nacl.sh
+++ nacl.sh
@@ -2,7 +2,7 @@ cd `dirname $0`
 
 #simple forced nacl build, just to see if we can get somewhere with this
 
-naclsdk=../naclsdk
+naclsdk=../sdks/native_client_sdk
 export PATH=$naclsdk/toolchain/linux_x86/nacl/bin:$PATH
 
 #so premake knows that it is a nacl build
