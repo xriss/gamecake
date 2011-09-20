939faf9de9ea035b326bef02b29f4997f2272c9e
kriss <devnull@localhost>
2011-09-20 20:45:45 +0100

    we need to scroll?

---
+++ fenestra/widget/scroll.lua
@@ -0,0 +1,40 @@
+-- widget class string
+-- a one line string buffer that can be edited
+
+
+
+local require=require
+local print=print
+
+module("fenestra.widget.string")
+
+local string=require("string")
+local table=require("table")
+
+function mouse(widget,act,x,y,key)
+--	widget.master.focus=widget
+	return widget.meta.mouse(widget,act,x,y,key)
+end
+
+
+function key(widget,ascii,key,act)
+	return widget.meta.key(widget,ascii,key,act)
+end
+
+
+function update(widget)
+	return widget.meta.update(widget)
+end
+
+
+function setup(widget,def)
+	local it={}
+	widget.string=it
+	widget.class="scroll"
+	
+	widget.key=key
+	widget.mouse=mouse
+	widget.update=update
+
+	return widget
+end
