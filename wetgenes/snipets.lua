2752f384231410eae289d61aee87b872f1303418
kriss <devnull@localhost>
2011-09-27 22:49:53 +0100

    local locals for local locals

---
+++ wetgenes/snipets.lua
@@ -0,0 +1,9 @@
+-- just snippets of code for copypasta do not run as it should do nothing
+-- this files is intended as a central place to keep upto date incase things need to be changed
+
+-- functions into locals
+local assert,dofile,error,_G,getfenv,getmetatable,ipairs,load,loadfile,loadstring,next,pairs,pcall,print,rawequal,rawget,rawset,select,setfenv,setmetatable,tonumber,tostring,type,unpack,_VERSION,xpcall,module,require=assert,dofile,error,_G,getfenv,getmetatable,ipairs,load,loadfile,loadstring,next,pairs,pcall,print,rawequal,rawget,rawset,select,setfenv,setmetatable,tonumber,tostring,type,unpack,_VERSION,xpcall,module,require
+
+-- libs into locals
+local coroutine,package,string,table,math,io,os,debug=coroutine,package,string,table,math,io,os,debug
+
