8fdc006428c95bbb3aaee44a83c74a1acdba5534
Kriss <kriss@XIXs.com>
2011-12-28 13:03:24 +0000

    ndk is not building properly so I think we can remove it...

--- nginx/objs/ngx_modules.c
+++ nginx/objs/ngx_modules.c
@@ -51,7 +51,7 @@ extern ngx_module_t  ngx_http_range_body_filter_module;
 extern ngx_module_t  ngx_http_not_modified_filter_module;
 
 extern ngx_module_t  ngx_http_lua_module;
-extern ngx_module_t  ndk_http_module;
+//extern ngx_module_t  ndk_http_module;
 
 ngx_module_t *ngx_modules[] = {
     &ngx_core_module,
@@ -101,7 +101,7 @@ ngx_module_t *ngx_modules[] = {
     &ngx_http_not_modified_filter_module,
     
     &ngx_http_lua_module,
-    &ndk_http_module,
+//    &ndk_http_module,
     NULL
 };
 
