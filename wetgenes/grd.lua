f5728f29c05fd244c2475cc1a8257db198988651
kriss <devnull@localhost>
2012-01-25 21:04:24 +0000

    grd has moved to wetgenes.grd

---
+++ wetgenes/grd.lua
@@ -0,0 +1,15 @@
+-- copy all globals into locals, some locals are prefixed with a G to reduce name clashes
+local coroutine,package,string,table,math,io,os,debug,assert,dofile,error,_G,getfenv,getmetatable,ipairs,Gload,loadfile,loadstring,next,pairs,pcall,print,rawequal,rawget,rawset,select,setfenv,setmetatable,tonumber,tostring,type,unpack,_VERSION,xpcall,module,require=coroutine,package,string,table,math,io,os,debug,assert,dofile,error,_G,getfenv,getmetatable,ipairs,load,loadfile,loadstring,next,pairs,pcall,print,rawequal,rawget,rawset,select,setfenv,setmetatable,tonumber,tostring,type,unpack,_VERSION,xpcall,module,require
+
+-- the core module previously lived in "grd" now it is in "wetgenes.grd.core" with this wrapper code
+
+module("wetgenes.grd")
+
+local core=require("wetgenes.grd.core")
+
+
+create=function(...)
+
+	return core.create(...)
+	
+end
