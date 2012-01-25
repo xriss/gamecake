199bf3bdbd2e1cd81f90f7849f581fb57c663011
kriss <kriss@localhost>
2012-01-25 21:04:57 +0000

    grd has moved to wetgenes.grd need to update all references

--- lua_grd/code/lua_grd.h
+++ lua_grd/code/lua_grd.h
@@ -10,7 +10,7 @@
 extern "C" {
 #endif
 
-LUALIB_API int luaopen_grd (lua_State *l);
+LUALIB_API int luaopen_wetgenes_grd_core (lua_State *l);
 
 #ifdef __cplusplus
 };
@@ -18,7 +18,7 @@ LUALIB_API int luaopen_grd (lua_State *l);
 
 
 
-#define LUA_grd_LIB_NAME "grd"
+#define LUA_grd_LIB_NAME "wetgenes.grd.core"
 
 extern const char *lua_grd_ptr_name;
 
