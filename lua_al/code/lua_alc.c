230c818a01897b87ca09666bb89b72519479e7c1
kriss <kriss@localhost>
2012-02-12 02:04:44 +0000

    gotsome device and context setup

--- lua_al/code/lua_alc.c
+++ lua_al/code/lua_alc.c
@@ -5,6 +5,15 @@
 #include <AL/al.h>
 #include <AL/alc.h>
 
+//
+// we can use either these strings as a string identifier
+// or the address as a light userdata identifier, both unique
+//
+const char *lua_alc_device_meta_name="alc*device*ptr";
+const char *lua_alc_context_meta_name="alc*context*ptr";
+
+
+
 
 /*
 alcCreateContext
@@ -51,9 +60,9 @@ for(i=0;i<512;i++)
 	
 int buffer;
 	
-ALCdevice* device = alcOpenDevice(NULL);
-ALCcontext* context = alcCreateContext(device, NULL);
-alcMakeContextCurrent(context);
+//ALCdevice* device = alcOpenDevice(NULL);
+//ALCcontext* context = alcCreateContext(device, NULL);
+//alcMakeContextCurrent(context);
 
 alListener3f(AL_POSITION, 0, 0, 0);
 alListener3f(AL_VELOCITY, 0, 0, 0);
@@ -82,8 +91,8 @@ fgetc(stdin);
 
 alDeleteSources(1, &source);
 alDeleteBuffers(1, &buffer);
-alcDestroyContext(context);
-alcCloseDevice(device);
+//alcDestroyContext(context);
+//alcCloseDevice(device);
 
 
 
@@ -91,6 +100,110 @@ alcCloseDevice(device);
   return 0;
 }
 
+/*+-----------------------------------------------------------------------------------------------------------------+*/
+//
+// open device
+//
+/*+-----------------------------------------------------------------------------------------------------------------+*/
+static int lua_alc_OpenDevice(lua_State *l)
+{
+	ALCdevice **ppdevice;
+
+// create a device userdata pointer pointer
+	ppdevice = (ALCdevice**)lua_newuserdata(l, sizeof(ALCdevice**));	
+	luaL_getmetatable(l, lua_alc_device_meta_name);
+	lua_setmetatable(l, -2);
+	
+//start pointer at 0
+	(*ppdevice)=0;
+
+//open the actual device
+	(*ppdevice)=alcOpenDevice(NULL);
+
+//return the userdata	
+	return 1;
+}
+/*+-----------------------------------------------------------------------------------------------------------------+*/
+//
+// __GC for device ptr
+//
+/*+-----------------------------------------------------------------------------------------------------------------+*/
+static int lua_alc_CloseDevice (lua_State *l)
+{	
+ALCdevice **ppdevice;
+
+	ppdevice = (ALCdevice**)luaL_checkudata(l, 1 , lua_alc_device_meta_name);
+	
+	if(*ppdevice)
+	{
+		alcCloseDevice(*ppdevice);
+		(*ppdevice)=0;
+	}
+	
+	return 0;
+}
+
+/*+-----------------------------------------------------------------------------------------------------------------+*/
+//
+// open device
+//
+/*+-----------------------------------------------------------------------------------------------------------------+*/
+static int lua_alc_CreateContext(lua_State *l)
+{
+	ALCdevice **ppdevice;
+	ALCcontext **ppcontext;
+
+// must pass in a device
+	ppdevice = (ALCdevice**)luaL_checkudata(l, 1 , lua_alc_device_meta_name);
+
+// create a context userdata pointer pointer
+	ppcontext = (ALCcontext**)lua_newuserdata(l, sizeof(ALCcontext**));	
+	luaL_getmetatable(l, lua_alc_context_meta_name);
+	lua_setmetatable(l, -2);
+	
+//start pointer at 0
+	(*ppcontext)=0;
+
+//open the actual context
+	(*ppcontext)=alcCreateContext((*ppdevice),NULL);
+
+//return the userdata	
+	return 1;
+}
+/*+-----------------------------------------------------------------------------------------------------------------+*/
+//
+// __GC for context ptr
+//
+/*+-----------------------------------------------------------------------------------------------------------------+*/
+static int lua_alc_DestroyContext (lua_State *l)
+{	
+ALCcontext **ppcontext;
+
+	ppcontext = (ALCcontext**)luaL_checkudata(l, 1 , lua_alc_context_meta_name);
+	
+	if(*ppcontext)
+	{
+		alcDestroyContext(*ppcontext);
+		(*ppcontext)=0;
+	}
+	
+	return 0;
+}
+/*+-----------------------------------------------------------------------------------------------------------------+*/
+//
+// __GC for context ptr
+//
+/*+-----------------------------------------------------------------------------------------------------------------+*/
+static int lua_alc_MakeContextCurrent (lua_State *l)
+{	
+ALCcontext **ppcontext;
+
+	ppcontext = (ALCcontext**)luaL_checkudata(l, 1 , lua_alc_context_meta_name);
+	
+	alcMakeContextCurrent(*ppcontext);
+	
+	return 0;
+}
 
 /*+-----------------------------------------------------------------------------------------------------------------+*/
 //
@@ -101,14 +214,37 @@ LUALIB_API int luaopen_alc_core(lua_State *l)
 {
 	const luaL_reg lib[] =
 	{
-		{"test",			lua_alc_test},
+		{"OpenDevice",			lua_alc_OpenDevice},
+		{"CloseDevice",			lua_alc_CloseDevice},
+
+		{"CreateContext",		lua_alc_CreateContext},
+		{"DestroyContext",		lua_alc_DestroyContext},
+		
+		{"MakeContextCurrent",	lua_alc_MakeContextCurrent},
 
 		{0,0}
 	};
+	const luaL_reg meta_device[] =
+	{
+		{"__gc",			lua_alc_CloseDevice},
+		{0,0}
+	};
+	const luaL_reg meta_context[] =
+	{
+		{"__gc",			lua_alc_DestroyContext},
+		{0,0}
+	};
+
+	luaL_newmetatable(l, lua_alc_device_meta_name);
+	luaL_openlib(l, NULL, meta_device, 0);
+	lua_pop(l,1);
+		
+	luaL_newmetatable(l, lua_alc_context_meta_name);
+	luaL_openlib(l, NULL, meta_context, 0);
+	lua_pop(l,1);
 
 	lua_newtable(l);
 	luaL_openlib(l, NULL, lib, 0);
-	
 	return 1;
 }
 
