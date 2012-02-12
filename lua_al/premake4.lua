1ef83ec05e607bd9558a93c5a10ea0dd8689d246
Kriss <kriss@XIXs.com>
2012-02-12 00:35:28 +0000

    merged al and alc asit shouldhave been to start with :)

--- lua_al/premake4.lua
+++ lua_al/premake4.lua
@@ -11,3 +11,16 @@ includedirs { "../lib_openal/soft/include"}
 
 KIND{lua="al.core"}
 
+
+project "lua_alc"
+language "C"
+
+files { "code/lua_alc.c" }
+
+links { "lib_lua" }
+
+includedirs { "../lib_openal/soft/include"}
+
+
+KIND{lua="alc.core"}
+
