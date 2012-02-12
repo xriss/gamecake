9e55d969ff5ab74bd06eb19a4c0b381cc6f158d0
kriss <kriss@localhost>
2012-02-12 15:20:51 +0000

    initial al bindings, this is enough to make some noise

--- apps/drytest/lua/test_lua_al.lua
+++ apps/drytest/lua/test_lua_al.lua
@@ -9,6 +9,40 @@ function test_al()
 local al=require("al")
 local alc=require("alc")
 
+	
+	local dc=alc.setup()
+	
+--	alc.test()-- test junk
+
+	local data="00000000ZZZZZZZZ" -- fake test sample data should be squarewave ishhh
+
+	al.Listener(al.POSITION, 0, 0, 0)
+	al.Listener(al.VELOCITY, 0, 0, 0)
+	al.Listener(al.ORIENTATION, 0, 0, -1, 0,1,0 )
+
+	local source=al.GenSource()
+
+	al.Source(source, al.PITCH, 1)
+	al.Source(source, al.GAIN, 1)
+	al.Source(source, al.POSITION, 0, 0, 0)
+	al.Source(source, al.VELOCITY, 0, 0, 0)
+	al.Source(source, al.LOOPING, al.FALSE)
+
+	local buffer=al.GenBuffer()
+
+	al.BufferData(buffer,al.FORMAT_MONO16,data,1024)
+
+	al.Source(source, al.BUFFER, buffer)
+	al.Source(source, al.LOOPING,al.TRUE)
+
+	al.SourcePlay(source)
+	alc.test()-- test junk
+
+	al.DeleteSource(source)
+	al.DeleteBuffer(buffer)
+	
+	dc:clean() -- should really clean up when finished
+
 	print("AL",wstr.dump(al))
 	print("ALC",wstr.dump(alc))
 	
