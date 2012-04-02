7db06c7bc0c055c2677f6b59b754977e49bbe318
kriss <kriss@localhost>
2012-04-02 15:32:25 +0100

    nacl is building and testing fine again, time to add the hard codes back in...

--- lib_nacl/premake4.lua
+++
@@ -1,9 +0,0 @@
-project "lib_nacl"
-
-language "C"
-
-files { "code/*.h" , "code/gltest.c" }
-
-links { "lib_lua" }
-
-KIND{}
--- nacl/code/gltest.c
+++ nacl/code/gltest.c
@@ -1,3 +1,5 @@
+
+
 /** @file nacl-gl-sample.c
  * This example demonstrates how to use the Graphics3D interface from C.
  */
@@ -20,10 +22,10 @@
 #include "GLES2/gl2.h"
 
 static PP_Module module_id = 0;
-static struct PPB_Messaging* messaging_interface = NULL;
-static struct PPB_Var* var_interface = NULL;
-static struct PPB_Graphics3D* graphics3d_interface = NULL;
-static struct PPB_Instance* instance_interface = NULL;
+static PPB_Messaging* messaging_interface = NULL;
+static PPB_Var* var_interface = NULL;
+static PPB_Graphics3D* graphics3d_interface = NULL;
+static PPB_Instance* instance_interface = NULL;
 
 void swap_callback(void* user_data, int32_t result)
 {
@@ -43,7 +45,6 @@ static PP_Bool Instance_DidCreate(PP_Instance instance,
     printf("failed to create graphics3d context\n");
     return PP_FALSE;
   }
-    printf("created graphics3d context\n");
 
   glSetCurrentContextPPAPI(context);
 
@@ -52,10 +53,10 @@ static PP_Bool Instance_DidCreate(PP_Instance instance,
     return PP_FALSE;
   }
 
-  glClearColor(0.0f, 0.0f, 0.5f, 0.0f);
+  glClearColor(1.0f, 0.9f, 0.4f, 0.9f);
   glClear(GL_COLOR_BUFFER_BIT);
 
-  struct PP_CompletionCallback callback = { swap_callback, NULL, PP_COMPLETIONCALLBACK_FLAG_NONE };
+   struct PP_CompletionCallback callback = { swap_callback, NULL, PP_COMPLETIONCALLBACK_FLAG_NONE };
   int32_t ret = graphics3d_interface->SwapBuffers(context, callback);
   if (ret != PP_OK && ret != PP_OK_COMPLETIONPENDING) {
     printf("SwapBuffers failed with code %d\n", ret);
@@ -82,19 +83,19 @@ static PP_Bool Instance_HandleDocumentLoad(PP_Instance instance,
   return PP_FALSE;
 }
 
-void Messaging_HandleMessage(PP_Instance instance, struct PP_Var var_message) {
+void Messaging_HandleMessage(PP_Instance instance,  struct PP_Var var_message) {
 }
 
 PP_EXPORT int32_t PPP_InitializeModule(PP_Module a_module_id,
                                        PPB_GetInterface get_browser_interface) {
   module_id = a_module_id;
-  var_interface = (struct PPB_Var*)(get_browser_interface(PPB_VAR_INTERFACE));
+  var_interface = ( PPB_Var*)(get_browser_interface(PPB_VAR_INTERFACE));
   messaging_interface =
-      (struct PPB_Messaging*)(get_browser_interface(PPB_MESSAGING_INTERFACE));
+      ( PPB_Messaging*)(get_browser_interface(PPB_MESSAGING_INTERFACE));
   graphics3d_interface =
-      (struct PPB_Graphics3D*)(get_browser_interface(PPB_GRAPHICS_3D_INTERFACE));
+      ( PPB_Graphics3D*)(get_browser_interface(PPB_GRAPHICS_3D_INTERFACE));
   instance_interface =
-      (struct PPB_Instance*)(get_browser_interface(PPB_INSTANCE_INTERFACE));
+      ( PPB_Instance*)(get_browser_interface(PPB_INSTANCE_INTERFACE));
 
   if (!glInitializePPAPI(get_browser_interface)) {
     printf("glInitializePPAPI failed\n");
@@ -106,7 +107,7 @@ PP_EXPORT int32_t PPP_InitializeModule(PP_Module a_module_id,
 
 PP_EXPORT const void* PPP_GetInterface(const char* interface_name) {
   if (strcmp(interface_name, PPP_INSTANCE_INTERFACE) == 0) {
-    static struct PPP_Instance instance_interface = {
+    static  PPP_Instance instance_interface = {
       &Instance_DidCreate,
       &Instance_DidDestroy,
       &Instance_DidChangeView,
@@ -115,7 +116,7 @@ PP_EXPORT const void* PPP_GetInterface(const char* interface_name) {
     };
     return &instance_interface;
   } else if (strcmp(interface_name, PPP_MESSAGING_INTERFACE) == 0) {
-    static struct PPP_Messaging messaging_interface = {
+    static  PPP_Messaging messaging_interface = {
       &Messaging_HandleMessage
     };
     return &messaging_interface;
