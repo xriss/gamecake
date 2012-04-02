ab44269f66eca7ec222f3baba953f5025a6d2e69
kriss <kriss@localhost>
2012-04-02 17:35:44 +0100

    building and linking on nacl, now we need to wire it up so it runs lua...

--- lib_jpeg/premake4.lua
+++ lib_jpeg/premake4.lua
@@ -4,6 +4,7 @@ kind "StaticLib"
 language "C++"
 files { "./**.cpp" , "./**.c" , "./**.h" }
 excludes { "jmemdos.c" ,  "jmemmac.c" , "ansi2knr.c" , "example.c" }
+excludes { "cjpeg.c", "djpeg.c" , "rdjpgcom.c" , "wrjpgcom.c" , "ckconfig.c" , "jpegtran.c" }
 
 defines { "JPEGSTATIC" }
 
--- lib_lua/premake4.lua
+++ lib_lua/premake4.lua
@@ -1,14 +1,19 @@
 
---if not NACL then -- we just link with prebuilt?
 
 project "lib_lua"
 language "C"
 files { "src/*.h", "src/*.cpp", "src/*.c" }
 excludes { "src/lua.c" }
+excludes { "src/luac.c" }
 includedirs { "src" }
 
 defines("LUA_PRELOADLIBS=lua_preloadlibs")
 
+if NACL then -- nacl needs its own flags
+
+else
+
+end
+
 KIND{}
 
---end
