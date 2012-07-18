7529d3eaa2178c94486a632e25e19e45643cb642
kriss <kriss@localhost>
2012-07-18 03:26:54 +0100

    moar simpler

--- apps/demo/gamecake.lua
+++ apps/demo/gamecake.lua
@@ -7,12 +7,19 @@ local coroutine,package,string,table,math,io,os,debug,assert,dofile,error,_G,get
 local apps=require("apps")
 apps.default_paths()
 
+local pack=require("wetgenes.pack")
 local wwin=require("wetgenes.win")
 local wstr=require("wetgenes.string")
 local tardis=require("wetgenes.tardis")	-- matrix/vector math
 
 local opts={
 	times=true, -- request simple time keeping samples
+	
+	
+	width=640,	-- display basics
+	height=480,
+	title="testing",
+	fps=60,
 }
 
 
@@ -21,14 +28,14 @@ function start()
 
 	do
 		local screen=wwin.screen()
-		local inf={width=640,height=480,title="testing"}
+		local inf={width=opts.width,height=opts.height,title=opts.title}
 		inf.x=(screen.width-inf.width)/2
 		inf.y=(screen.height-inf.height)/2
 		state.win=wwin.create(inf)
 		state.gl=require("gles").gles1
 		state.win:context({})
 
-		state.frame_rate=1/60 -- how fast we want to run
+		state.frame_rate=1/opts.fps -- how fast we want to run
 		state.frame_time=0
 
 		require("wetgenes.gamecake").bake({
@@ -82,14 +89,14 @@ end
 
 demo.draw=function(state)
 --print("draw")
-	local win=state.win
 	local cake=state.cake
 	local canvas=state.canvas
 	local font=canvas.font
+	local flat=canvas.flat
 	local gl=cake.gl
 	
 	canvas:viewport() -- did our window change?
-	canvas:project23d(640,480,0.25,480*4)
+	canvas:project23d(opts.width,opts.height,0.25,opts.height*4)
 	canvas:gl_default() -- reset gl state
 		
 	gl.ClearColor(0,0,0,0)
@@ -100,14 +107,19 @@ demo.draw=function(state)
 
 	gl.MatrixMode(gl.MODELVIEW)
 	gl.LoadIdentity()
-	gl.Translate(-320,-240,-480*2) -- a good starting point
+	gl.Translate(-opts.width/2,-opts.height/2,-opts.height*2) -- a good starting point
 
 	gl.PushMatrix()
 	
+	gl.Color(pack.argb8_pmf4(0xff0000ff))
+	flat:quad(0,(opts.height/2)-32,opts.width,(opts.height/2)+32)
+
+	gl.Color(pack.argb8_pmf4(0xffffffff))
 	font:set(cake.fonts:get(1))
 	font:set_size(32,0)
-	font:set_xy((640-(12*32))/2,240-16)
+	font:set_xy((opts.width-(12*32))/2,(opts.height-32)/2)
 	font:draw("Hello World!")
+
 	
 	gl.PopMatrix()
 	
