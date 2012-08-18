612cbb3fb216cec7ecb939cddababadbf8ec0059
kriss <kriss@localhost>
2012-08-18 14:56:02 +0100

    windows gl working again but only gles2 style...

--- apps/demo/test.lua
+++ apps/demo/test.lua
@@ -6,6 +6,9 @@ require("apps").default_paths()
 local wstr=require("wetgenes.string")   -- string helpers
 --local tardis=require("wetgenes.tardis") -- matrix/vector math
 
+-- wrap some extra shader compiler functions around a basic gles2 library
+local gl=require("glescode").create( assert(require("gles").gles2) )
+
 -- a window/screen handler this works in windows/linux/nacl/android/raspi
 
 local wwin=require("wetgenes.win")
@@ -13,6 +16,9 @@ local wwin=require("wetgenes.win")
 print( wstr.dump(wwin.screen()) )
 
 local win=wwin.create({})
+
+-- select a standard gles context
+win:context({})
 
 print(wstr.dump(win))
 
@@ -23,5 +29,15 @@ for i=1,1000 do
 	if m then
 		print(wstr.dump(m))
 	end
+
+	
+	win:info()
+	gl.Viewport(0,0,win.width,win.height)
+
+	gl.ClearColor(0,0,1/4,1/4)
+	gl.Clear(gl.COLOR_BUFFER_BIT+gl.DEPTH_BUFFER_BIT)
+
+	win:swap()
+
 end
 
