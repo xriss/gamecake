6c87731e44a552efc26c6b1fc9704ad3722b40f0
kriss <kriss@localhost>
2012-08-25 03:03:16 +0100

    mycroft?

--- lua_mycroft/code/lua_mycroft.h
+++ lua_mycroft/code/lua_mycroft.h
@@ -1,6 +1,7 @@
 
 
 // this is going to be a bit wobbiliy and redefinable, somehow...
+// however its size is always 1x1x1 
 typedef struct {
 	
 	u32		id;
@@ -8,40 +9,33 @@ typedef struct {
 } myc_cell;
 
 
+// every byte in this structure expands to 32k per block
 typedef struct {
 	
-	s32		px;		// position (within parent)
-	s32		py;
+	u16		idx;	// index into parent data so bottom 15 bits are zzzzzyyyyyxxxxx
+	u16		flags;
 	
-	s32		hx;		// size (number of cells) probably 8x8
-	s32		hy;
-	
-	myc_cell default_cell[1];	// if data is 0 then all cells in this block are...	
-	
-	void	*data;
+	myc_cell	*data; // possible pointer to array of 8x8x8 (512) cells, or 0 if this block is just empty
 	
 } myc_block;
 
 typedef struct {
 	
-	s32		px;		// position (within parent)
+	s32		px;		// position, within world, of this chunk
 	s32		py;
+	s32		pz;
 	
-	s32		hx;		// size (number of blocks) probably 32*32
-	s32		hy;
-	
-	myc_cell default_block[1];	// if data is 0 then all blocks in this chunk are...	
-
-	void	*data;
+	myc_block	blocks[32*32*32]; // an array of 32*32*32 (32768) blocks
 
 } myc_chunk;
 
 typedef struct {
 	
-	s32		hx;		// size (number of chunkc)
+	s32		hx;		// size (defined in chunks) so 1x1x1 is 32x32x32 blocks or 256x256x256 cells
 	s32		hy;
+	s32		hz;
 	
-	void	*data;
+	myc_chunk	**chunks; // array of pointers to chunks
 
 } myc_world;
 
