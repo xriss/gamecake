4a3b567d69facbddb994dcc6c90812000e2a233f
kriss <kriss@localhost>
2012-09-10 11:09:35 +0100

    androi buikd has to embed an app from somewhere

--- android/bake.lua
+++ android/bake.lua
@@ -1,21 +1,51 @@
 #!/usr/local/bin/gamecake
 
-local wbake=require("wetgenes.bake")
-local wzips=require("wetgenes.zips")
+local zips=require("wetgenes.zips")
+local bake=require("wetgenes.bake")
+local sbox=require("wetgenes.sandbox")
 local wstr=require("wetgenes.string")
 
+local args={...}
+
+local basedir=assert(args[1],"Must specify basedir, eg \"./bake.lua ../../apps/dike\"")
+
+
+local fdat=assert(zips.readfile(basedir.."/opts.lua"),"opts.lua must exist in the given basedir")
+
+
+local opts=sbox.ini(fdat)
+
+--print(wstr.dump(opts))
+
+local version=bake.version_from_time()
+local opts={
+        name=opts.title,
+        namev=opts.title..".v"..version,
+        version=version,
+        version_int=math.floor(version*1000),
+}
+
+bake.replacefile("AndroidManifest.xml.base","AndroidManifest.xml",opts)
+bake.replacefile("build.xml.base","build.xml",opts)
+
+-- remove all files in res/raw
+
+os.execute("rm -rf res/raw")
+
+-- copy all data and code from root into res/raw
 
 for _,dir in ipairs{"lua","data"} do
 
-	local files=wbake.findfiles{basedir="..",dir=dir,filter="."}.ret
+	local files=bake.findfiles{basedir=basedir,dir=dir}.ret
 
 	for i,v in ipairs(files) do
-		local n=wzips.apk_munge_filename(v)
-		wbake.create_dir_for_file("res/"..n)
-		wbake.copyfile("../"..v,"res/"..n)
+		local n=zips.apk_munge_filename(v) -- this includes the res/raw prefix
+		bake.create_dir_for_file(n)
+		bake.copyfile(basedir.."/"..v,n)
 		print(n)
 	end
 
 end
+
 
 
