ec5761cbaf30efc88cf5d49fd1e5839102a61edb
Kriss <kriss@XIXs.com>
2012-11-09 19:53:59 +0000

    better speach

--- lua_voice/rsynth/ASCII.h
+++
@@ -1,3 +0,0 @@
-/* $Id: ASCII.h,v 1.13 1994/11/08 13:30:50 a904209 Exp a904209 $
-*/
-extern char *ASCII[];
--- lua_voice/rsynth/h2pw.h
+++
@@ -1,8 +0,0 @@
-#ifdef  JONS_PARWAVE 
-#include "parwave.h"
-extern klatt_frame_t def_pars;
-extern klatt_global_t klatt_global;
-extern int init_synth PROTO((int argc,char *argv[]));
-#else
-#include "nsynth.h"
-#endif
