962c38c8f158d2fbe85827a87c4ad15a0252ff10
Kriss <kriss@XIXs.com>
2012-11-11 02:36:08 +0000

    mild voice setup

--- lua_speak/premake4.lua
+++ lua_speak/premake4.lua
@@ -12,7 +12,8 @@ files { "flite/voices/cmulex/**.c" , "flite/voices/cmulex/**.h" }
 files { "flite/voices/usenglish/**.c" , "flite/voices/usenglish/**.h" }
 
 -- choose the voices that are baked in
-files { "flite/voices/cmu_us_kal/**.c" , "flite/voices/cmu_us_kal/**.h" }
+files { "flite/voices/cmu_us_slt/**.c" , "flite/voices/cmu_us_slt/**.h" }
+--files { "flite/voices/cmu_us_kal/**.c" , "flite/voices/cmu_us_kal/**.h" }
 
 
 includedirs { "." , "flite/include" , "flite/voices/usenglish" , "flite/voices/cmulex" }
