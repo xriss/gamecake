79d84622cf17bbe5d9c3afa9b4252af7e6a929e3
kriss <kriss@localhost>
2012-11-11 02:36:08 +0000

    mild voice setup

--- lua_speak/code/lua_speak.c
+++ lua_speak/code/lua_speak.c
@@ -8,8 +8,8 @@
 
 #include "flite.h"
 
-//extern cst_voice *register_cmu_us_slt();
-extern cst_voice *register_cmu_us_kal();
+extern cst_voice *register_cmu_us_slt();
+//extern cst_voice *register_cmu_us_kal();
 
 static int init=0;
 static cst_voice *voice=0;
@@ -20,13 +20,16 @@ static int lua_speak_setup (lua_State *l)
 	if(!init)
 	{
 		flite_init();
-	//	voice=register_cmu_us_slt();
-		voice=register_cmu_us_kal();
-		
-		
+		voice=register_cmu_us_slt();
+//		voice=register_cmu_us_kal();
+
+//    flite_feat_set_float(voice->features,"int_f0_target_mean",95.0);
+//    flite_feat_set_float(voice->features,"int_f0_target_stddev",11.0*1);
+//    flite_feat_set_float(voice->features,"duration_stretch",1.1);
+
 		init=1;
 	}
-	
+		
 	return 0;
 }
 
@@ -35,17 +38,95 @@ static int lua_speak_setup (lua_State *l)
 static int lua_speak_text (lua_State *l)
 {
 cst_wave *sound;
-u16 *data;
+s16 *data;
 const char *text;
+int len;
+
+s16 *p;
+s16 *pend;
+
+int d;
+
+
+s16 *p_start;
+s16 *p_stop;
+int cbeg,cend;
+int clips=0;
 
 	lua_speak_setup(l);
 
 	text=luaL_checkstring(l,1);
 	sound = flite_text_to_wave(text, voice);
+		
+	data=sound->samples;
+	pend=data+sound->num_samples;
 	
+//	int mp=1;
+//	int ph=32;
+//	int m=1;
+	for(p=data;p<pend;p++)
+	{
+		d=*p;
+		d=d*3;//m/ph;
+		
+//		if(d>0x7fff) { d=0xffff-d; clips++; }
+//		if(d<-0x7fff) { d=-0xffff+d; clips++; }
+
+		if(d>0x7fff) { d=0x7fff; }
+		if(d<-0x7fff) { d=-0x7fff; }
+
+		*p=(s16)d;
+		
+//		m=m+mp;
+//		if(m==1) {mp=1;}
+//		if(m==ph*16) {mp=-1;}
+	}
+//printf("clips %d\n",clips);
+
+
+#define samptrim (1024)
+	p_start=0;
+	p_stop=data-1;
+	for(p=data;p<pend;p++)
+	{
+		d=*p;
+		if((d>samptrim)||(d<-samptrim))
+		{
+			if(!p_start) { p_start=p; } // first non zero sample
+		}
+	}
+	if(!p_start) { p_start=p; } // first non zero sample
+	
+	p_stop=pend-1;
+/*	
+	for(p=pend-1;p>=data;p--)
+	{
+		d=*p;
+		if((d>samptrim)||(d<-samptrim))
+		{
+			p_stop=p; // last non zero sample
+			break;
+		}
+	}
+*/
+	
+	cbeg=p_start-data;
+	cend=pend-(p_stop+1);
+
+printf("blank space %d %d / %d\n",cbeg,cend,sound->num_samples);
+
+	
+	len=(p_stop+1-p_start)*2;
+	if(len<1) {len=1;p_start=data;}
+	data = (u16*)lua_newuserdata(l,len);
+	memcpy(data,p_start,len);
+	lua_pushnumber(l,len);
+
+/*
 	data = (u16*)lua_newuserdata(l,sound->num_samples*2);
 	memcpy(data,sound->samples,sound->num_samples*2);
 	lua_pushnumber(l,sound->num_samples*2);
+*/
 
 	delete_wave(sound);
 
@@ -65,6 +146,10 @@ const char *text;
 static int lua_speak_voice (lua_State *l)
 {
 	lua_speak_setup(l);
+
+    flite_feat_set_float(voice->features,"int_f0_target_mean",300);
+    flite_feat_set_float(voice->features,"int_f0_target_stddev",100);
+    flite_feat_set_float(voice->features,"duration_stretch",0.75);
 
 	return 0;
 }
