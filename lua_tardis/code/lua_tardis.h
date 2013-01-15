2c5e6942bb1883014bf2e96f24d6a93730f66d6c
kriss <kriss@localhost>
2013-01-15 11:01:31 +0000

    add some C tardis codes

---
+++ lua_tardis/code/lua_tardis.h
@@ -0,0 +1,23 @@
+/*
+-- Copyright (C) 2012 Kriss Blank < Kriss@XIXs.com >
+-- This file is distributed under the terms of the MIT license.
+-- http://en.wikipedia.org/wiki/MIT_License
+-- Please ping me if you use it for anything cool...
+--
+-- Time And Relative Dimensions In Space
+--
+-- Best, 3dmathlib, name, ever.
+--
+*/
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+LUALIB_API int luaopen_wetgenes_tardis_core (lua_State *l);
+
+#ifdef __cplusplus
+};
+#endif
+
