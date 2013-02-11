aae8d379b1d031e8ee71ce77f4aaad38a6c2f901
kriss <kriss@localhost>
2013-02-11 10:20:19 +0000

    3d test

--- apps/test/test2.lua
+++ apps/test/test2.lua
@@ -13,6 +13,8 @@ local wxox=require("wetgenes.xox")
 local wstr=require("wetgenes.string")
 local tardis=require("wetgenes.tardis")	-- matrix/vector math
 
+local startbake
+
 local opts={
 	times=true, -- request simple time keeping samples
 	
@@ -20,146 +22,116 @@ local opts={
 	height=480,
 	title="testing",
 	fps=60,
+
 }
 
-local demo={} -- a demo state
+opts.start=function(oven,demo)
 
-local bake=function()
-	local state=require("wetgenes.gamecake.state").bake(opts)
+	demo.loads=function()
 
-	do
-		local screen=wwin.screen()
-		local inf={width=opts.width,height=opts.height,title=opts.title}
-		inf.x=(screen.width-inf.width)/2
-		inf.y=(screen.height-inf.height)/2
-		state.win=wwin.create(inf)
-		state.gl=require("gles").gles1
-		state.win:context({})
+		oven.cake.fonts.loads({1}) -- load 1st builtin font, a basic 8x8 font
 
-		state.frame_rate=1/opts.fps -- how fast we want to run
-		state.frame_time=0
+	end
+			
+	demo.setup=function()
 
-		require("wetgenes.gamecake").bake({
-			state=state,
-			width=inf.width,
-			height=inf.height,
-		})
+		demo.loads(oven)
+
+
+		demo.xox=wxox.create_xox({}):load_dae("cog.dae")
 		
 	end
-	
-	state.require_mod("wetgenes.gamecake.mods.escmenu") -- escmenu gives us a doom style escape menu
-	state.require_mod("wetgenes.gamecake.mods.console") -- console gives us a quake style tilda console
-
-	state.next=demo -- we want to run a demo state
-
-	return state
-end
 
 
-demo.loads=function(state)
+	demo.clean=function()
 
-	state.cake.fonts.loads({1}) -- load 1st builtin font, a basic 8x8 font
+	end
 
-end
+	demo.msg=function(m)
+	end
+
+	demo.update=function()
+	end
+
+	local ii=0
+	demo.draw=function()
+
+		ii=ii+1
 		
-demo.setup=function(state)
-
-	demo.loads(state)
-
-
-	demo.xox=wxox.create_xox({}):load_dae("cog.dae")
-	
-end
-
-
-demo.clean=function(state)
-
-end
-
-demo.msg=function(state,m)
-end
-
-demo.update=function(state)
-end
-
-local ii=0
-demo.draw=function(state)
-
-	ii=ii+1
-	
---print("draw")
-	local cake=state.cake
-	local canvas=state.canvas
-	local font=canvas.font
-	local flat=canvas.flat
-	local gl=cake.gl
-	
-	canvas.viewport() -- did our window change?
-	canvas.project23d(opts.width,opts.height,0.25,opts.height*4)
-	canvas.gl_default() -- reset gl state
+	--print("draw")
+		local cake=oven.cake
+		local canvas=cake.canvas
+		local font=canvas.font
+		local flat=canvas.flat
+		local gl=oven.gl
 		
-	gl.ClearColor(0,0,0,0)
-	gl.Clear(gl.COLOR_BUFFER_BIT+gl.DEPTH_BUFFER_BIT)
+		canvas.viewport() -- did our window change?
+		canvas.project23d(opts.width,opts.height,0.25,opts.height*4)
+		canvas.gl_default() -- reset gl state
+			
+		gl.ClearColor(0,0,0,0)
+		gl.Clear(gl.COLOR_BUFFER_BIT+gl.DEPTH_BUFFER_BIT)
 
-	gl.MatrixMode(gl.PROJECTION)
-	gl.LoadMatrix( canvas.pmtx )
+		gl.MatrixMode(gl.PROJECTION)
+		gl.LoadMatrix( canvas.pmtx )
 
-	gl.MatrixMode(gl.MODELVIEW)
-	gl.LoadIdentity()
-	gl.Translate(-opts.width/2,-opts.height/2,-opts.height*2) -- top left corner is origin
+		gl.MatrixMode(gl.MODELVIEW)
+		gl.LoadIdentity()
+		gl.Translate(-opts.width/2,-opts.height/2,-opts.height*2) -- top left corner is origin
 
-	gl.PushMatrix()
-	
+		gl.PushMatrix()
+		
 
-	gl.Color(pack.argb4_pmf4(0x800f))
-	flat.quad(0,(opts.height/2)-32,opts.width,(opts.height/2)+32) -- draw a blue box as a background
+		gl.Color(pack.argb4_pmf4(0x800f))
+		flat.quad(0,(opts.height/2)-32,opts.width,(opts.height/2)+32) -- draw a blue box as a background
 
-	font.set(cake.fonts.get(1)) -- default font
-	font.set_size(32,0) -- 32 pixels high
-	
-	local s="Hello World!"
-	local sw=font.width(s) -- how wide the string is
-	local x,y=(opts.width-(sw))/2,(opts.height-32)/2 -- center the text in  middle of display
+		font.set(cake.fonts.get(1)) -- default font
+		font.set_size(32,0) -- 32 pixels high
+		
+		local s="Hello World!"
+		local sw=font.width(s) -- how wide the string is
+		local x,y=(opts.width-(sw))/2,(opts.height-32)/2 -- center the text in  middle of display
 
-	gl.Color(pack.argb4_pmf4(0xf000)) -- draw drop shadow
-	font.set_xy(x+4,y+4)
-	font.draw(s)
-	
-	gl.Color(pack.argb4_pmf4(0xffff)) -- draw white text
-	font.set_xy(x,y)
-	font.draw(s)
+		gl.Color(pack.argb4_pmf4(0xf000)) -- draw drop shadow
+		font.set_xy(x+4,y+4)
+		font.draw(s)
+		
+		gl.Color(pack.argb4_pmf4(0xffff)) -- draw white text
+		font.set_xy(x,y)
+		font.draw(s)
 
 
-	gl.Color(pack.argb4_pmf4(0xffff)) -- draw white text
-	canvas.flat.tristrip("xyzrgba",{
-		100,100,0, 1,0,0,1,
-		200,100,0, 0,1,0,1,
-		100,200,0, 0,0,1,1,
-		})
-	
-	gl.Translate(200,200,0)
---	gl.Scale(300,300,300)
-	gl.Scale(100,100,100)
-	gl.Rotate(ii,1,1,0)
---	gl.Rotate(ii,1,0,0)
-	
-	gl.Enable(gl.DEPTH_TEST)
---	gl.Enable(gl.CULL_FACE)
+		gl.Color(pack.argb4_pmf4(0xffff)) -- draw white text
+		canvas.flat.tristrip("xyzrgba",{
+			100,100,0, 1,0,0,1,
+			200,100,0, 0,1,0,1,
+			100,200,0, 0,0,1,1,
+			})
+		
+		gl.Translate(200,200,0)
+	--	gl.Scale(300,300,300)
+		gl.Scale(100,100,100)
+		gl.Rotate(ii,1,1,0)
+	--	gl.Rotate(ii,1,0,0)
+		
+		gl.Enable(gl.DEPTH_TEST)
+	--	gl.Enable(gl.CULL_FACE)
 
---	demo.draw_test(state)
-	demo.xox:draw_canvas(canvas)
+	--	demo.draw_test(state)
+		demo.xox:draw_canvas(canvas)
 
-	gl.Disable(gl.DEPTH_TEST)
---	gl.Disable(gl.CULL_FACE)
-	
-	gl.PopMatrix()
-	
+		gl.Disable(gl.DEPTH_TEST)
+	--	gl.Disable(gl.CULL_FACE)
+		
+		gl.PopMatrix()
+		
+	end
+
+	return demo
 end
-
-
 
 -- this will busy loop or hand back control depending on system we are running on
-return bake():serv()
+return require("wetgenes.gamecake.oven").bake(opts).preheat():serv()
 
 
 
