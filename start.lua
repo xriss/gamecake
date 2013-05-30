6faf8a188e4d8eeaadb2b7b5c6cb425e0f7f3f91
kriss <devnull@localhost>
2013-05-30 15:25:12 +0100

    gamecake -lstart name.zip gamecake name.zip
    will now mount the zip file and run lua/init.lua from inside it
    
    be careful with this as it still writes saves into the current directory

--- start.lua
+++ start.lua
@@ -4,24 +4,41 @@ local apps=require("apps")
 -- try hard to find any files wemay need
 apps.default_paths()
 
--- we expect to find the initial file here
+local wzips=require("wetgenes.zips")
 
-local name="lua/init.lua"
-local fp=assert(io.open(name))
-local str=fp:read("*a")
-fp:close()
+-- strip some args before passing on to main code
+local a=arg or {}
+local argx={}
 
-if str:sub(1,2)=="#!" then
-	str="--"..str -- ignore hashbang on first line
+local done_start=false -- only remove the first
+local done_zip=false
+
+for i=1,#a do
+	local v=tostring(a[i])
+
+	if v=="-lstart" and not done_start then
+		done_start=true
+		v=nil
+	elseif v:sub(-4)==".zip" and not done_zip then
+		wzips.add_zip_file(v)
+		done_zip=true	
+		v=nil
+	end
+
+	if v then argx[#argx+1]=v end 
 end
-local func=assert(loadstring(str,name))
 
--- finally call with args, skiping everything until -lstart
-local argx=arg or {}
-local argi=1
-for i,v in ipairs(argx) do
-	if tostring(v)=="-lstart" then argi=i+1 break end
+local str=wzips.readfile("lua/init.lua")
+
+if str then
+
+	if str:sub(1,2)=="#!" then
+		str="--"..str -- ignore hashbang on first line
+	end
+
+	local func=assert(loadstring(str,name))
+
+	func(unpack(argx))
+
+	os.exit(0) -- force close so that we do not end up at a console?
 end
-func(unpack(argx,argi))
-
-os.exit(0) -- force close so that we do not end up at a console?
