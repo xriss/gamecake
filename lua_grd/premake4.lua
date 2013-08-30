e0b3332bbfd31873ba867b702635ad08958645b0
Kriss <kriss@XIXs.com>
2013-08-30 07:39:04 +0100

    moved libpng and libjpg up a directory.

--- lua_grd/premake4.lua
+++ lua_grd/premake4.lua
@@ -8,7 +8,7 @@ links { "lib_lua" , "lib_png" , "lib_z" }
 
 defines { "JPEGSTATIC" }
 
-includedirs { "." , "../lib_z" , "../lib_png/lpng163" , "../lib_jpeg/jpeg-6b" }
+includedirs { "." , "../lib_z" , "../lib_png" , "../lib_jpeg" }
 
 
 KIND{kind="lua",dir="wetgenes/grd",name="core",luaname="wetgenes.grd.core",luaopen="wetgenes_grd_core"}
