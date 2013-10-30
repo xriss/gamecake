5d8f1c3770042e580526b56bf4be6fdb34847622
kriss <kriss@localhost>
2013-10-30 22:36:08 +0000

    .

--- lib_gif/premake4.lua
+++ lib_gif/premake4.lua
@@ -4,12 +4,12 @@ project "lib_gif"
 language "C"
 files { "giflib/lib/**.c" , "giflib/lib/**.h" }
 
-excludes { "giflib/lib/getarg.c" } 
-excludes { "giflib/lib/qprintf.c" } 
+--excludes { "giflib/lib/getarg.c" } 
+--excludes { "giflib/lib/qprintf.c" } 
 
 links { "lib_z" }
 
-defines { "HAVE_CONFIG_H" }
+--defines { "HAVE_CONFIG_H" }
 
 includedirs { "." , "../lib_z" }
 
