f090073fd4f55a374c4d77eacdad5fe86d23a866
Kriss <kriss@XIXs.com>
2014-04-24 12:34:42 +0100

    64bit input packets are just weird

--- test/old/hid.lua
+++ test/old/hid.lua
@@ -16,14 +16,23 @@ local fp=assert(posix.open("/dev/input/event12", posix.O_NONBLOCK + posix.O_RDON
 while true do
 	wwin.sleep(0.0001)
 
-	local pkt=posix.read(fp,16)
+--	local pkt=posix.read(fp,16) -- 32bit
+	local pkt=posix.read(fp,24) -- 64bit hax
+
+--print(pkt)
 	
 	if pkt then
 	
-		 local tab=pack.load(pkt,{"u32","secs","u32","micros","u16","type","u16","code","u32","value"})
-		
-		print(tab.type,tab.code,tab.value)
-	
+		local tab
+		if #pkt==24 then
+			tab=pack.load(pkt,{"u32","secs","u32","secs64","u32","micros","u32","micros64","u16","type","u16","code","u32","value"})
+		elseif #pkt==16 then
+			tab=pack.load(pkt,{"u32","secs","u32","micros","u16","type","u16","code","u32","value"})
+		end
+
+		if tab then
+			print(tab.secs,tab.type,tab.code,tab.value)
+		end
 	end
 	
 	
