4dba7401c9ca625e63d5ec243c621ee33ffde920
Kriss <kriss@XIXs.com>
2014-05-15 17:24:50 +0100

    ok polarssl seem to be happy, might actually be able to talk to https clients now...

--- lib_polarssl/premake4.lua
+++ lib_polarssl/premake4.lua
@@ -1,9 +1,9 @@
-
 project "lib_polarssl"
-kind "StaticLib"
 language "C"
 files { "./library/**.c" , "./library/**.h" }
+
 defines { "POLARSSL_HAVEGE_C" }
+
 includedirs { "." , "include" }
 
 KIND{}
--- lua_polarssl/premake4.lua
+++ lua_polarssl/premake4.lua
@@ -2,6 +2,8 @@ project "lua_polarssl"
 language "C"
 files { "./src/luapolarssl.c" }
 
+defines { "POLARSSL_HAVEGE_C" }
+
 includedirs { "./src" , "../lib_polarssl/include" }
 
 KIND{lua="polarssl"}
