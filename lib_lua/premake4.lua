b22755cef5e3a2a78b53244bfc5e1602309f35cb
Kriss <kriss@XIXs.com>
2014-05-15 21:29:22 +0100

    readline fixes

--- lib_lua/premake4.lua
+++ lib_lua/premake4.lua
@@ -1,5 +1,4 @@
 
-
 project "lib_lua"
 language "C"
 files { "src/*.h", "src/*.cpp", "src/*.c" }
@@ -8,15 +7,6 @@ excludes { "src/luac.c" }
 includedirs { "src" }
 
 defines("LUA_PRELOADLIBS=lua_preloadlibs")
-
-if NIX then -- nacl needs its own flags
-
-if LSB then -- readline is bad for lsb, actually readline is just bad but simple to use
-else
---defines("LUA_USE_READLINE")
-end
-
-end
 
 KIND{}
 
