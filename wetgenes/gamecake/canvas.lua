6995c2698af29b42991f4c0f91fe767031043e1a
kriss <devnull@localhost>
2014-08-02 22:59:39 +0100

    fix point sprites on desktop gl

--- wetgenes/gamecake/canvas.lua
+++ wetgenes/gamecake/canvas.lua
@@ -46,6 +46,8 @@ canvas.gl_default=function()
 	gl.BlendFunc(gl.ONE, gl.ONE_MINUS_SRC_ALPHA)
 	gl.Enable(gl.BLEND)
 	
+	gl.Enable(0x8642) -- #define GL_PROGRAM_POINT_SIZE 0x8642
+			
 	gl.MatrixMode(gl.MODELVIEW)
 
 end
