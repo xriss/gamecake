8ce6e86066deb8d43d0bf60bb15a8059d3907db8
kriss <kriss@localhost>
2015-01-21 20:23:30 +0000

    .

--- lib_pq/premake4.lua
+++ lib_pq/premake4.lua
@@ -41,22 +41,6 @@ files {
 	"./src/interfaces/libpq/encnames.c",	
 	"./src/interfaces/libpq/wchar.c",	
 	}
---[[
-# We can't use Makefile variables here because the MSVC build system scrapes
-# OBJS from this file.
-OBJS=	fe-auth.o fe-connect.o fe-exec.o fe-misc.o fe-print.o fe-lobj.o \
-	fe-protocol2.o fe-protocol3.o pqexpbuffer.o fe-secure.o \
-	libpq-events.o
-# libpgport C files we always use
-OBJS += chklocale.o inet_net_ntop.o noblock.o pgstrcasecmp.o pqsignal.o \
-	thread.o
-# libpgport C files that are needed if identified by configure
-OBJS += $(filter crypt.o getaddrinfo.o getpeereid.o inet_aton.o open.o system.o snprintf.o strerror.o strlcpy.o win32error.o win32setlocale.o, $(LIBOBJS))
-# backend/libpq
-OBJS += ip.o md5.o
-# utils/mb
-OBJS += encnames.o wchar.o
-]]
 
 defines{ "FRONTEND" , "UNSAFE_STAT_OK" }
 
