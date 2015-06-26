35ba75d84ebd0e2d1e6e8370eeefc6f8b22a5561
Kriss <kriss@XIXs.com>
2015-06-26 20:39:09 +0100

    move to a _PRELOAD function in the registry rather than luajit patch

--- lib_luajit/src/lib_init.c
+++ lib_luajit/src/lib_init.c
@@ -36,9 +36,6 @@ static const luaL_Reg lj_lib_preload[] = {
   { NULL,		NULL }
 };
 
-#ifdef LUA_PRELOADLIBS
-  extern void LUA_PRELOADLIBS(lua_State *L);
-#endif
 LUALIB_API void luaL_openlibs(lua_State *L)
 {
   const luaL_Reg *lib;
@@ -47,9 +44,6 @@ LUALIB_API void luaL_openlibs(lua_State *L)
     lua_pushstring(L, lib->name);
     lua_call(L, 1, 0);
   }
-#ifdef LUA_PRELOADLIBS
-  LUA_PRELOADLIBS(L);
-#endif
   luaL_findtable(L, LUA_REGISTRYINDEX, "_PRELOAD",
 		 sizeof(lj_lib_preload)/sizeof(lj_lib_preload[0])-1);
   for (lib = lj_lib_preload; lib->func; lib++) {
