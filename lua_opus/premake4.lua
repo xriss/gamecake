5eb01c81b78b010a795b3f11ba61706ed51276b3
Kriss <kriss@XIXs.com>
2015-08-14 00:57:01 +0100

    win console fix

--- lua_opus/premake4.lua
+++ lua_opus/premake4.lua
@@ -5,6 +5,8 @@ files { "code/**.cpp" , "code/**.c" , "code/**.h" , "all.h" }
 
 links { "lib_lua" }
 
+defines { "HAVE_STDINT_H" }  -- the speexdsp include needs this
+
 includedirs { "." , "../lib_opus/include" , "../lib_speexdsp/include" }
 
 KIND{lua="wetgenes.opus.core"}
