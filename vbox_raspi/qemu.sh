81070d0b641bf5cd3c7f5913e65242f230ce97c1
Kriss <kriss@XIXs.com>
2015-09-07 23:23:49 +0100

    .

--- vbox_raspi/qemu.sh
+++ vbox_raspi/qemu.sh
@@ -1,3 +1,3 @@
 
-qemu-system-arm -kernel kernel-qemu -cpu arm1176 -m 256 -M versatilepb -no-reboot -serial stdio -append "root=/dev/sda2 panic=1" -hda 2015-02-18-wheezy-minibian.img -redir tcp:5022::22
+qemu-system-arm -kernel kernel-qemu -cpu arm1176 -m 256 -M versatilepb -no-reboot -append "root=/dev/sda2 panic=1 console=ttyAMA0  console=ttyS0" -hda 2015-02-18-wheezy-minibian.img -redir tcp:5022::22 -nographic
 
