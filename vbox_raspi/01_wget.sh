c04049463216f0ec16be95a286109a1a35977c09
Kriss <kriss@XIXs.com>
2015-09-07 21:42:05 +0100

    .

--- vbox_raspi/01_wget.sh
+++ vbox_raspi/01_wget.sh
@@ -1,7 +1,26 @@
+cd `dirname $0`
 
-wget -o minibian.img.tar.gz http://sourceforge.net/projects/minibian/files/2015-02-18-wheezy-minibian.tar.gz/download
-tar xvfz minibian.img.tar.gz
+if [ -f 2015-02-18-wheezy-minibian.img ] ; then
 
-wget https://github.com/dhruvvyas90/qemu-rpi-kernel/raw/master/kernel-qemu
+	echo " 2015-02-18-wheezy-minibian.img exists so skipping download and unpack"
+
+else
+
+	wget -o minibian.img.tar.gz http://sourceforge.net/projects/minibian/files/2015-02-18-wheezy-m inibian.tar.gz/download
+	tar xvfz minibian.img.tar.gz
+
+fi
+
+if [ -f kernel-qemu ] ; then
+
+	echo " kernel-qemu exists so skipping download "
+
+else
+
+	wget https://github.com/dhruvvyas90/qemu-rpi-kernel/raw/master/kernel-qemu
+
+fi
+
+echo " installing qemu "
 
 sudo apt-get install qemu-system
