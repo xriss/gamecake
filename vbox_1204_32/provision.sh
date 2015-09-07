90b4491c54c864a66b252883905e23e0d46c321c
Kriss <kriss@XIXs.com>
2015-09-07 21:33:34 +0100

    vagrant settings

---
+++ vbox_1204_32/provision.sh
@@ -0,0 +1,10 @@
+
+sudo apt-get update
+
+sudo apt-get install -y build-essential libreadline-dev libgl1-mesa-dev libx11-dev libasound2-dev libudev-dev
+
+sudo apt-get install -y gcc-arm-linux-gnueabihf
+sudo apt-get install -y g++-arm-linux-gnueabihf
+
+
+
---
+++ vbox_raspi/01_wget.sh
@@ -0,0 +1,7 @@
+
+wget -o minibian.img.tar.gz http://sourceforge.net/projects/minibian/files/2015-02-18-wheezy-minibian.tar.gz/download
+tar xvfz minibian.img.tar.gz
+
+wget https://github.com/dhruvvyas90/qemu-rpi-kernel/raw/master/kernel-qemu
+
+sudo apt-get install qemu-system
