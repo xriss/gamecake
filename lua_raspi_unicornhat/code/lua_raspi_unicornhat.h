56f94391a7a1c49c95571855b1e5e1cc24be1c10
Kriss <kriss@XIXs.com>
2015-12-01 09:27:51 +0000

    unicorn hat lib for the raspi

---
+++ lua_raspi_unicornhat/code/lua_raspi_unicornhat.h
@@ -0,0 +1,22 @@
+/*+-----------------------------------------------------------------------------------------------------------------+*/
+//
+// (C) Kriss@XIXs.com 2015
+//
+/*+-----------------------------------------------------------------------------------------------------------------+*/
+
+
+
+extern const char *lua_raspi_unicornhat_ptr_name;
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+LUALIB_API	int luaopen_raspi_unicornhat (lua_State *l);
+
+
+#ifdef __cplusplus
+};
+#endif
