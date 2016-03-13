8c3129d0273f13fa4f535456c235b468529957f7
kriss <devnull@localhost>
2016-03-13 02:03:10 +0000

    dox

--- wetgenes/grd.lua
+++ wetgenes/grd.lua
@@ -3,6 +3,94 @@
 --
 local coroutine,package,string,table,math,io,os,debug,assert,dofile,error,_G,getfenv,getmetatable,ipairs,Gload,loadfile,loadstring,next,pairs,pcall,print,rawequal,rawget,rawset,select,setfenv,setmetatable,tonumber,tostring,type,unpack,_VERSION,xpcall,module,require=coroutine,package,string,table,math,io,os,debug,assert,dofile,error,_G,getfenv,getmetatable,ipairs,load,loadfile,loadstring,next,pairs,pcall,print,rawequal,rawget,rawset,select,setfenv,setmetatable,tonumber,tostring,type,unpack,_VERSION,xpcall,module,require
 
+--[[#wetgenes.grd
+
+	local wgrd=require("wetgenes.grd")
+
+We use wgrd as the local name of this library.
+
+Handle bitmap creation, loading, saving and blitting. The bitmap and 
+the colormap for indexed bitmaps are represented by the same data 
+structure which describes a continuous chunk of memory with optional 
+ability to select an area of a larger chunk using simple byte spans.
+
+Swanky Paint uses this to manage its bitmaps and its also used to 
+convert art into data at build time for use in the GameCake engine. The 
+PageCake engine uses this for image management, creating live thumbnails 
+and so on.
+
+We load and save jpeg, png and gif. The png lib contains extensions for 
+apng which allows animation chunks. Animations are contained in the Z 
+(depth) dimension of the grd.
+
+The following are possible format options that we support. Most of them 
+are OpenGL friendly.
+
+	wgrd.FMT_U8_RGBA
+	
+32 bits per pixel with a byte order of red, green, blue, alpha and a 
+little endian U32 of ABGR. We prefer this byte order because OpenGL.
+
+	wgrd.FMT_U8_ARGB
+
+32 bits per pixel with a byte order of alpha, red, green, blue and a 
+little endian U32 of BGRA.
+
+	wgrd.FMT_U8_RGB
+
+24 bits per pixel with a byte order of red, green, blue.
+
+	wgrd.FMT_U8_INDEXED
+	
+8 bits per pixel which indexes a wgrd.FMT_U8_RGBA palette.
+
+	wgrd.FMT_U8_LUMINANCE
+
+8 bits per pixel, grey scale only.
+
+	wgrd.FMT_U8_ALPHA
+
+8 bits per pixel, alpha only.
+
+	wgrd.FMT_U16_RGBA_5551
+
+16 bits per pixel with 5 bits each of red,green,blue and 1 bit of alpha 
+packed into a single U16.
+
+	wgrd.FMT_U16_RGBA_4444
+
+16 bits per pixel with 4 bits each of red,green,blue,alpha packed into 
+a single U16.
+
+	wgrd.FMT_U16_RGBA_5650
+
+16 bits per pixel with 4 bits of red, 5 bits of green and 4 bits of 
+blue packed into a single U16.
+
+
+	wgrd.FMT_U8_RGBA_PREMULT
+	wgrd.FMT_U8_ARGB_PREMULT
+	wgrd.FMT_U8_INDEXED_PREMULT
+	wgrd.FMT_U16_RGBA_5551_PREMULT
+	wgrd.FMT_U16_RGBA_4444_PREMULT
+	wgrd.FMT_U16_RGBA_5650_PREMULT
+
+Are all just pre-multiplied alpha versions of the base format described 
+above.
+
+Check this link out for why pre-multiplied alpha is a good idea for any 
+image that will be used as a texture 
+http://blogs.msdn.com/b/shawnhar/archive/2009/11/06/premultiplied-alpha.aspx
+
+
+	wgrd.GRD_FMT_HINT_PNG
+	wgrd.GRD_FMT_HINT_JPG
+	wgrd.GRD_FMT_HINT_GIF
+
+These are used to choose a specific file format when loading or saving.
+
+]]
+
 -- the core module previously lived in "grd" now it is in "wetgenes.grd.core" with this wrapper code
 
 local grd={}
@@ -183,11 +271,38 @@ function grd.stringtonum(str)
 	return grd.defs[str] 
 end
 
+--[[#wetgenes.grd.create
+
+	ga=wgrd.create(gb)
+
+Duplicate the grd.
+	
+	ga=wgrd.create(format,width,height,depth)
+
+Create a grd in the given format with the given width height and depth. 
+
+	ga=wgrd.create(filename,opts)
+
+Load an image from the file system.
+
+	ga=wgrd.create()
+	
+Create a blank grd of 0 dimensions.
+
+This is usually the only wgrd function you would need to call as once you 
+have a grd you can use the : calling convention to modify it. The other 
+functions will be shown as examples using the : calling convention.
+
+	wgrd.create():load(opts)
+
+For instance could be used if you wish to perform a more esoteric load 
+than from the file system.
+
+Returns nil,error if something goes wrong so can be used with assert 
+otherwise returns a grd object.
+
+]]
 -- many options
--- grd.create(g) -- duplicate
--- grd.create(fmt,w,h,d) -- a new blank image of given dimensions
--- grd.create(filename,opts) -- load image
--- grd.create() -- a blank image of 0 dimensions
 grd.create=function(...)
 	local args={...}
 	local g={}
@@ -233,25 +348,89 @@ grd.create=function(...)
 	return g
 end
 
+--[[#wetgenes.grd.destroy
+
+	g:destroy()
+
+Free the grd and associated memory. This will of course be done 
+automatically by garbage collection but you can force it explicitly 
+using this function.
+	
+]]
 base.destroy=function(g)
 	return core.destroy(g[0])
 end
 
 
+--[[#wetgenes.grd.reset
+
+	g:reset()
+
+Reset the grd which will now be a blank image of 0 dimensions.
+
+Returns nil,error if something goes wrong so can be used with assert 
+otherwise returns g so that we can chain the result.
+
+]]
 base.reset=function(g)
 	local r=core.reset(g[0])
 	core.info(g[0],g)
-	return r and g
+	return (r and g),g.err
 end
 
+--[[#wetgenes.grd.load_file
+
+	g:load_file(filename,format)
+
+Load an image from the file system.
+
+Returns nil,error if something goes wrong so can be used with assert 
+otherwise returns g so that we can chain the result.
+ 
+]]
 base.load_file=function(g,filename,fmt)
 	return base.load(g,{filename=filename,fmt=fmt})
 end
 
+--[[#wetgenes.grd.load_data
+
+	g:load_data(datastring,format)
+
+Load an image from memory.
+ 
+Returns nil,error if something goes wrong so can be used with assert 
+otherwise returns g so that we can chain the result.
+
+]]
 base.load_data=function(g,data,fmt)
 	return base.load(g,{data=data,fmt=fmt})
 end
 
+--[[#wetgenes.grd.load
+
+	g:load(opts)
+
+Load an image from memory or file system depending on settings in opts.
+
+	opts.fmt
+
+Lets you choose an image format, the strings "jpg","png" or "gif" will 
+be converted to the appropriate wgrd.FMT_HINT_* value.
+
+	opts.data
+
+Flags this as a load from memory and provides the data string to load 
+from.
+
+	opts.filename
+
+Flags this as a load the file system and provides the file name to 
+open.
+ 
+Returns nil,error if something goes wrong so can be used with assert 
+otherwise returns g so that we can chain the result.
+
+]]
 base.load=function(g,opts)
 	if type(opts)=="string" then
 		opts={filename=opts}
@@ -270,6 +449,31 @@ base.load=function(g,opts)
 	return (r and g),g.err
 end
 
+--[[#wetgenes.grd.save
+
+	g:save(opts)
+
+Save an image to memory or filesytem depending on settings in opts.
+
+	opts.fmt
+
+Lets you choose an image format, the strings "jpg","png" or "gif" will 
+be converted to the appropriate wgrd.FMT_HINT_* value.
+
+We will guess opts.fmt from the file name extension if it is not 
+provided and a file name is.
+
+	opts.filename
+
+Flags this as a load the file system and provides the file name to 
+write to. If no filename is given then we will be saving into memory 
+and be returning that data string as the first return value.
+ 
+Returns nil,error if something goes wrong so can be used with assert.
+
+If no file name is given then we *return* the data string that we saved.
+
+]]
 base.save=function(g,opts)
 
 	if type(opts)=="string" then
@@ -310,10 +514,30 @@ base.save=function(g,opts)
 	return r,m -- first value may be a datastring if no filename was given or g[0] if it was,
 end
 
+--[[#wetgenes.grd.duplicate
+
+	ga = g:duplicate()
+
+Create a duplicate of this grd and return it.
+
+Returns nil,error if something goes wrong so can be used with assert 
+otherwise returns g so that we can chain the result.
+
+]]
 base.duplicate=function(g)
 	return grd.create(g)
 end
 
+--[[#wetgenes.grd.duplicate_convert
+
+	ga = g:duplicate_convert(fmt)
+
+Create a duplicate of this grd converted to a new format and return it.
+
+Returns nil,error if something goes wrong so can be used with assert 
+otherwise returns g so that we can chain the result.
+
+]]
 base.duplicate_convert=function(g,fmt)
 	if type(fmt) == "string" then
 		fmt=grd.stringtonum(fmt)
@@ -323,6 +547,17 @@ base.duplicate_convert=function(g,fmt)
 	return grd.create(r)
 end
 
+--[[#wetgenes.grd.convert
+
+	g:convert(fmt)
+
+Convert this grd to a new format, eg start with an 8 bit indexed grd 
+and convert it to 32 bit like by passing in wgrd.FMT_U8_RGBA as the fmt.
+
+Returns nil,error if something goes wrong so can be used with assert 
+otherwise returns g so that we can chain the result.
+
+]]
 base.convert=function(g,fmt)
 	if type(fmt) == "string" then
 		fmt=grd.stringtonum(fmt)
@@ -332,30 +567,131 @@ base.convert=function(g,fmt)
 	return (r and g),e
 end
 
+--[[#wetgenes.grd.clear
+
+	g:clear(color)
+
+Fill this grd with a single color, the color passed in depends on the 
+format of the grd, it could be an index value for 8bit images or a 
+32bit value for rgba images.
+
+Returns nil,error if something goes wrong so can be used with assert 
+otherwise returns g so that we can chain the result.
+
+]]
 base.clear=function(g,color)
 	local r=core.clear(g[0],color)
 	core.info(g[0],g)
 	return r and g
 end
 
+--[[#wetgenes.grd.quant
+
+	g:quant(num)
+
+Convert to an 8bit indexed image containing a palette of the requested size.
+
+Returns nil,error if something goes wrong so can be used with assert 
+otherwise returns g so that we can chain the result.
+
+]]
 base.quant=function(g,num)
 	local r=core.quant(g[0],num)
 	core.info(g[0],g)
 	return r and g
 end
 
+--[[#wetgenes.grd.attr_redux
+
+	g:attr_redux(cw,ch,num,sub,bak)
+
+Perform attribute clash simulation on an indexed image.
+
+cw,ch are the width and height of each character we are simulating, 8x8 
+is the right size for spectrum attrs but could be 4x8 for c64 multicolor 
+mode.
+
+num is the number of colors allowed within this area, so 2 for spectrum mode.
+
+sub is the size of sub pallete groups, eg 16 in nes mode or 8 in 
+spectrum mode, EG bright simulation in spectrum mode requires all 
+colors in a attr block to be from the bright palette or the dark 
+palette no mixing so this forces that grouping. Set to 0 or 256 and 
+this restriction will be disabled.
+
+bak is the index of the background color that is shared across all 
+characters, set to -1 if there is no shared background color.
+
+Returns nil,error if something goes wrong so can be used with assert 
+otherwise returns g so that we can chain the result.
+
+]]
 base.attr_redux=function(g,cw,ch,num,sub,bak)
 	local r=core.attr_redux(g[0],cw,ch,num,sub,bak)
 	core.info(g[0],g)
 	return r and g
 end
 
+--[[#wetgenes.grd.pixels
+
+	g:pixels(x,y,w,h)
+	g:pixels(x,y,z,w,h,d)
+
+Read the area of pixels as a table of numerical values, the amount of 
+numbers you get per pixel *depends* on the format of the grd.
+
+	g:pixels(x,y,w,h,"")
+	g:pixels(x,y,z,w,h,d,"")
+
+Read the area of pixels as a string of byte values, the amount of bytes 
+you get per pixel *depends* on the format of the grd. Note the passing 
+in of an empty string to indicate that you with to receive a string.
+
+
+	g:pixels(x,y,w,h,tab)
+	g:pixels(x,y,z,w,h,d,tab)
+
+Write the area of pixels from a table of numerical values which is 
+provided in tab, the amount of numbers you need to provide per pixel 
+*depends* on the format of the grd.
+
+	g:pixels(x,y,w,h,str)
+	g:pixels(x,y,z,w,h,d,str)
+
+Write the area of pixels from a string of bytes which is provided in 
+str, the amount of bytes you need to provide per pixel *depends* on the 
+format of the grd.
+
+As you can see depending on the arguments given this does one of two 
+things, read some pixels or write some pixels. The area that is to be 
+used is provided first, as a 2d(x,y,w,h) or 3d(x,y,z,w,h,d) area. To 
+read or write the entire 2d image or the first frame of an animation 
+use (0,0,g.width,g.height)
+
+Returns nil,error if something goes wrong so can be used with assert 
+otherwise returns the requested data.
+
+]]
 base.pixels=function(g,...)
 	local r=core.pixels(g[0],...)
 	core.info(g[0],g)
 	return r
 end
 
+--[[#wetgenes.grd.palette
+
+	g:palette(x,w)
+	g:palette(x,w,"")
+	g:palette(x,w,tab)
+	g:palette(x,w,str)
+
+These are the same as g:pixels() but refer to the palette information 
+which is stored as a 1 pixel high 256 pixel wide rgba image. The use of 
+"" to read a string of bytes and passing in either a table of numerical 
+values or string of bytes to write into the palette is the same system 
+as used with pixels.
+
+]]
 base.palette=function(g,...)
 	if not g.cmap then return nil end
 	local r=core.palette(g[0],...)
@@ -363,40 +699,130 @@ base.palette=function(g,...)
 	return r
 end
 
+--[[#wetgenes.grd.resize
+
+	g:resize(w,h,d)
+	
+Resize the image to the given dimensions, this does not scale the image 
+data so instead each pixel will be in the same place after the resize. 
+This gives a crop effect when shrinking and extra blank area at the 
+bottom right when growing. Useful if for instance you want to upload a 
+texture to OpenGL and need to change the size to be a power of two in 
+width and height so you can mipmap it.
+	
+]]
 base.resize=function(g,...)
 	local r=core.resize(g[0],...)
 	core.info(g[0],g)
 	return r and g
 end
+
+--[[#wetgenes.grd.scale
+
+	g:scale(w,h,d)
+	
+Scale the image to the given dimensions, this is currently using a 
+terrible scale filter that is only any good at halving or doubling the 
+size.
+
+This should only be used to create mipmaps until it is replaced with a 
+better scale filter.
+	
+]]
 base.scale=function(g,...)
 	local r=core.scale(g[0],...)
 	core.info(g[0],g)
 	return r and g
 end
 
+--[[#wetgenes.grd.flipy
+
+	g:flipy()
+	
+This function flips the image reversing the y axis.
+
+Some image formats and rendering engines like to use upside down images 
+so this is rather useful.
+
+]]
 base.flipy=function(g,...)
 	local r=core.flipy(g[0],...)
 	core.info(g[0],g)
 	return r and g
 end
 
+--[[#wetgenes.grd.flipy
+
+	g:flipx()
+	
+This function flips the image reversing the x axis.
+	
+]]
 base.flipx=function(g,...)
 	local r=core.flipx(g[0],...)
 	core.info(g[0],g)
 	return r and g
 end
 
+--[[#wetgenes.grd.shrink
+
+	g:shrink(area)
+
+area is an {x=0,y=0,z=0,w=100,h=100,d=100} style table specifying a 3D
+area, set {z=0,d=1} for a 2D area.
+
+You should set this to the full size of the image.
+
+This function looks at the pixels in that area and shrinks each edge 
+inwards if it is fully transparent then return this new area in the 
+same table that was passed in.
+
+You can then use this information to crop this image resulting is a 
+smaller sized grd containing all the solid pixels.
+
+]]
 base.shrink=function(g,...)
 	local r=core.shrink(g[0],...)
 	core.info(g[0],g)
 	return r and g
 end
 
+--[[#wetgenes.grd.info
+
+	g:info()
+
+This function looks at the userdata stored in g[0] and fills in the g 
+table with its values. So it refreshes the width height etc values to 
+reflect any changes. This should not need to be called explicitly as it 
+is called whenever we change anything.
+
+]]
 base.info=function(g,...)
 	core.info(g[0],g)
 	return g
 end
 
+--[[#wetgenes.grd.blit
+
+	g:blit(gb,x,y,cx,cy,cw,ch)
+
+Blit a 2D area from one grd into another.
+
+gb is the grd to blit from.
+
+x,y is the location to blit too.
+
+cx,cy,cw,ch is a clip area that should be applied to gb before it is 
+blitted. EG to specify an area within gb. If not provided it will 
+default to the entirety of gb,
+
+g (destination) must be FMT_U8_RGBA and gb (source) must be 
+FMT_U8_RGBA_PREMULT this function will blend the images using normal 
+alpha blending.
+
+This is not overly optimised but should be reasonably fast.
+
+]]
 base.blit=function(ga,gb,x,y,cx,cy,cw,ch)
 
 	if cx then -- autoclip
@@ -421,6 +847,55 @@ base.blit=function(ga,gb,x,y,cx,cy,cw,ch)
 	return core.blit(ga[0],gb[0],x,y,cx,cy,cw,ch)
 end
 
+--[[#wetgenes.grd.paint
+
+	g:paint(gb,x,y,cx,cy,cw,ch,mode,trans,color)
+
+Blit a 2D area from one grd into another using dpaint style paint modes.
+
+Both grids must be indexed - FMT_U8_INDEXED
+
+gb is the grd to blit from.
+
+x,y is the location to blit too.
+
+cx,cy,cw,ch is a clip area that should be applied to gb before it is 
+blitted. EG to specify an area within gb. If not provided it will 
+default to the entirety of gb,
+
+mode is one of the following
+
+	PAINT_MODE_TRANS
+	
+Skip the transparent color.
+
+	GRD_PAINT_MODE_COLOR
+	
+Skip the transparent color and make every solid pixel the same color.
+
+	GRD_PAINT_MODE_COPY
+
+Copy the entire area.
+
+	GRD_PAINT_MODE_XOR
+
+XOR the values. (Can be used to find differences in an image)
+
+	GRD_PAINT_MODE_ALPHA
+
+Skip the transparent colors as defined in the palette.
+
+
+trans is the index of the transparent color, bground color, for use in 
+the appropriate modes.
+
+color is the index of the drawing color, foreground color, for use in 
+the appropriate modes. 
+
+
+This is not overly optimised but should be reasonably fast.
+
+]]
 base.paint=function(ga,gb,x,y,cx,cy,cw,ch,mode,trans,color)
 
 	if cx then -- autoclip
@@ -445,14 +920,46 @@ base.paint=function(ga,gb,x,y,cx,cy,cw,ch,mode,trans,color)
 	return core.paint(ga[0],gb[0],x,y,cx,cy,cw,ch,mode,trans,color)
 end
 
+--[[#wetgenes.grd.copy_data
+
+	g:copy_data(gb)
+
+Copy all of the bitmap data from gb into g.
+
+]]
 base.copy_data=function(ga,gb)
 	return core.copy_data(ga[0],gb[0])
 end
+--[[#wetgenes.grd.copy_data_layer
+
+	g:copy_data_layer(gb,z,zb)
+
+Copy one layer (frame) of the bitmap data from gb into g. z is the 
+depth of the layer to copy into zb is the depth of the layer to copy 
+from.
+
+]]
 base.copy_data_layer=function(ga,gb,za,zb)
 	return core.copy_data_layer(ga[0],gb[0],za,zb)
 end
 
+--[[#wetgenes.grd.clip
 
+	gr=g:clip(x,y,z,w,h,d)
+
+create a clipped window into this grd
+
+the actual data is still stored in the original, so any changes there will effect the newly returned grd
+
+x,y,z are the staring pixel and w,h,d are the width height and depth in pixels.
+
+If you intend to use this clipped area for an extended period of time then you should duplicate this grd once you do this.
+
+This returns a new grd with gr.parent set to g (the original grd)
+
+This is a very shallow dangerous copy and should only really be used for temporary actions.
+
+]]
 base.clip=function(ga,x,y,z,w,h,d)
 	local g={}
 	g.parent=ga -- help make sure this master grd stays alive
