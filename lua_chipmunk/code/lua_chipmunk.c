75e720d4da061e51d6b36406f203286a92c03580
kriss <kriss@localhost>
2016-08-22 18:14:57 +0100

    some basic binds added

--- lua_chipmunk/code/lua_chipmunk.c
+++ lua_chipmunk/code/lua_chipmunk.c
@@ -87,6 +87,8 @@ cpBody **pp;
 static int lua_chipmunk_body_create (lua_State *l)
 {	
 cpBody **pp;
+const char *tp;
+double bm,bi;
 
 // create ptr ptr userdata
 	pp=(cpBody**)lua_newuserdata(l, sizeof(cpBody*));
@@ -95,7 +97,29 @@ cpBody **pp;
 	lua_setmetatable(l, -2);
 
 // allocate cpBody
-//	*pp=cpBodyNew();
+	if(lua_isnumber(l,1)) // dynamic if given mass and inertia
+	{
+		bm=luaL_checknumber(l,1);
+		bi=luaL_checknumber(l,2);
+		*pp=cpBodyNew(bm,bi);
+	}
+	else
+	{
+		tp=luaL_checkstring(l,1);
+		if(0==strcmp(tp,"kinematic"))
+		{
+			*pp=cpBodyNewKinematic();
+		}
+		else
+		if(0==strcmp(tp,"static"))
+		{
+			*pp=cpBodyNewStatic();
+		}
+		else
+		{
+			lua_pushstring(l,"unknown body type"); lua_error(l);
+		}
+	}
 
 	return 1;
 }
@@ -133,8 +157,13 @@ cpShape **pp;
 
 static int lua_chipmunk_shape_create (lua_State *l)
 {	
+const char *tp;
 cpShape **pp;
-
+cpBody *body;
+double cr,cx,cy;
+double sr,sxa,sya,sxb,syb;
+cpBB bb;
+double br;
 // create ptr ptr userdata
 	pp=(cpShape**)lua_newuserdata(l, sizeof(cpShape*));
 	(*pp)=0;
@@ -142,7 +171,46 @@ cpShape **pp;
 	lua_setmetatable(l, -2);
 
 // allocate cpShape
-//	*pp=cpShapeNew();
+		tp=luaL_checkstring(l,1);
+		if(0==strcmp(tp,"circle"))
+		{
+			body=lua_chipmunk_body_ptr(l,2);
+			cr=luaL_checknumber(l,3);
+			cx=luaL_checknumber(l,4);
+			cy=luaL_checknumber(l,5);
+			*pp=cpCircleShapeNew(body,cr,cpv(cx,cy));
+		}
+		else
+		if(0==strcmp(tp,"segment"))
+		{
+			body=lua_chipmunk_body_ptr(l,2);
+			sxa=luaL_checknumber(l,3);
+			sya=luaL_checknumber(l,4);
+			sxb=luaL_checknumber(l,5);
+			syb=luaL_checknumber(l,6);
+			sr=luaL_checknumber(l,7);
+			*pp=cpSegmentShapeNew(body,cpv(sxa,sya),cpv(sxb,syb),sr);
+		}
+//		else
+//		if(0==strcmp(tp,"poly"))
+//		{
+//			*pp=cpPolyShapeNew();
+//		}
+		else
+		if(0==strcmp(tp,"box"))
+		{
+			body=lua_chipmunk_body_ptr(l,2);
+			bb.l=luaL_checknumber(l,3);
+			bb.b=luaL_checknumber(l,4);
+			bb.r=luaL_checknumber(l,5);
+			bb.t=luaL_checknumber(l,6);
+			br=luaL_checknumber(l,7);
+			*pp=cpBoxShapeNew2(body,bb,br);
+		}
+		else
+		{
+			lua_pushstring(l,"unknown shape type"); lua_error(l);
+		}
 
 	return 1;
 }
@@ -156,6 +224,69 @@ cpShape **pp=lua_chipmunk_shape_ptr_ptr(l, 1 );
 		(*pp)=0;
 	}	
 	return 0;
+}
+
+
+/*+-----------------------------------------------------------------------------------------------------------------+*/
+//
+// space get/set iterations
+//
+/*+-----------------------------------------------------------------------------------------------------------------+*/
+static int lua_chipmunk_space_iterations (lua_State *l)
+{	
+cpSpace *space=lua_chipmunk_space_ptr(l,1);
+
+	if(lua_isnumber(l,2))
+	{
+		cpSpaceSetIterations(space,(int) luaL_checknumber(l,2) );
+	}
+	
+	lua_pushnumber(l, cpSpaceGetIterations(space) );
+
+	return 1;
+}
+
+/*+-----------------------------------------------------------------------------------------------------------------+*/
+//
+// space get/set gravity
+//
+/*+-----------------------------------------------------------------------------------------------------------------+*/
+static int lua_chipmunk_space_gravity (lua_State *l)
+{	
+cpSpace *space=lua_chipmunk_space_ptr(l,1);
+cpVect v;
+
+	if(lua_isnumber(l,2))
+	{
+		v.x=luaL_checknumber(l,2);
+		v.y=luaL_checknumber(l,3);
+		cpSpaceSetGravity(space,v);
+	}
+	
+	v=cpSpaceGetGravity(space);
+	lua_pushnumber(l,v.x);
+	lua_pushnumber(l,v.y);
+
+	return 2;
+}
+
+/*+-----------------------------------------------------------------------------------------------------------------+*/
+//
+// space get/set damping
+//
+/*+-----------------------------------------------------------------------------------------------------------------+*/
+static int lua_chipmunk_space_damping (lua_State *l)
+{	
+cpSpace *space=lua_chipmunk_space_ptr(l,1);
+
+	if(lua_isnumber(l,2))
+	{
+		cpSpaceSetDamping(space, luaL_checknumber(l,2) );
+	}
+	
+	lua_pushnumber(l, cpSpaceGetDamping(space) );
+
+	return 1;
 }
 
 /*+-----------------------------------------------------------------------------------------------------------------+*/
@@ -175,12 +306,19 @@ LUALIB_API int luaopen_wetgenes_chipmunk_core (lua_State *l)
 
 		{"shape_create",			lua_chipmunk_body_create},
 		{"shape_destroy",			lua_chipmunk_body_destroy},
+		
+		{"space_iterations",		lua_chipmunk_space_iterations},
+		{"space_gravity",			lua_chipmunk_space_gravity},
+		{"space_damping",			lua_chipmunk_space_damping},
 
 		{0,0}
 	};
 
 	const luaL_reg meta_space[] =
 	{
+		{"iterations",		lua_chipmunk_space_iterations},
+		{"gravity",			lua_chipmunk_space_gravity},
+		{"damping",			lua_chipmunk_space_damping},
 		{"__gc",			lua_chipmunk_space_destroy},
 		{0,0}
 	};
