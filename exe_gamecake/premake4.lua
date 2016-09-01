e45905784143049487fac6f07c0f1d4947c775d9
kriss <kriss@localhost>
2016-09-01 01:32:05 +0100

    mingw fixs slighlty more stable builds

--- exe_gamecake/premake4.lua
+++ exe_gamecake/premake4.lua
@@ -106,7 +106,9 @@ elseif WINDOWS then
 	files { "./lua.c" }
 
 	libdirs { "../lib_sdl2/win32/i686-w64-mingw32/lib/" } -- we have SDL2 binary for windows
-	linkoptions { " -Wl,-Bstatic,-lSDL2,-lm,-ldinput8,-ldxguid,-ldxerr8,-luser32,-lgdi32,-lwinmm,-limm32,-lole32,-loleaut32,-lshell32,-lversion,-luuid,-Bdynamic " }  -- force static SDL2 linking
+--	linkoptions { " -Wl,-Bstatic,-lSDL2,-lm,-ldinput8,-ldxguid,-ldxerr8,-luser32,-lgdi32,-lwinmm,-limm32,-lole32,-loleaut32,-lshell32,-lversion,-luuid,-Bdynamic " }  -- force static SDL2 linking
+
+	links { "SDL2" }
 
 	links { "opengl32" , "glu32" }
 	links {  "ws2_32" , "gdi32"}
