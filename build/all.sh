22c025b82e708e4f99637523b9cb6def875ae2b4
kriss <kriss@xixs.com>
2016-11-15 21:31:44 +0000

    fix bin path

--- build/all.sh
+++ build/all.sh
@@ -1,42 +1,42 @@
 cd `dirname $0`
 
 #prepare
-../../bin/revert_exe
+#../exe/revert_exe
 
 #build windows
 
-../../lua/mingw/make clean
-../../lua/mingw/make release gamecake
-
-#build raspi
-
-../../lua/vbox_raspi/make
+../mingw/make clean
+../mingw/make release gamecake
 
 #build nacl
 
-../../lua/nacl/make clean
-../../lua/nacl/make release gamecake
+../nacl/make clean
+../nacl/make release gamecake
 
 #build emscipten
 
-../../lua/emcc/make clean
-../../lua/emcc/make release gamecake
+../emcc/make clean
+../emcc/make release gamecake
 
 # build 64bit ubuntu 12.04 version
 
-../../lua/vbox_1204_64/make
+../vbox_1204_64/make
 
 #build 32bit ubuntu 12.04 version 
 
-../../lua/vbox_1204_32/make
+../vbox_1204_32/make
 
 #build osx version 64 (dont think anything uses 32 anymore)
 
-../../lua/vbox_osx/make
+../vbox_osx/make
+
+#build raspi
+
+../vbox_raspi/make
 
 #compress files
 
-upx -9 ../../bin/exe/*
+upx -9 ../exe/*
 
 
 #show status, this should list new versions of everything we just built
