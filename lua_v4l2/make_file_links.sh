ce439313ed4a6909ed4762e25b072ce56ee6e204
kriss <kriss@xixs.com>
2016-11-20 02:25:16 +0000

    merged file link scrips

--- lua_v4l2/make_file_links.sh
+++
@@ -1,6 +0,0 @@
-echo "linking .lua fles from the main lua dir so we dont accidently edit multiple copies"
-
-#you will need to delete the local copies first...
-#rm code/wetgenes/v4l2.lua
-
-ln ../../bin/lua/wetgenes/v4l2.lua code
