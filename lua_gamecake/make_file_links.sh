9eaf54b4abea04284f1af1e67f1dc20df40dcc3d
kriss <kriss@xixs.com>
2016-12-03 15:05:59 +0000

    remve out of date duplicate code

--- lua_gamecake/make_file_links.sh
+++
@@ -1,6 +0,0 @@
-echo "linking .lua fles from the main lua dir so we dont accidently edit multiple copies"
-
-#you will need to delete the local copies first...
-#rm code/wetgenes/pack.lua
-
-ln -v ../../bin/lua/wetgenes/gamecake/*.lua code
