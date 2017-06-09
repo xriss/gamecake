cfdbfec72a7054b55b0fb1636f86e8c63d830fb5
kriss <kriss@xixs.com>
2017-06-09 03:48:17 +0100

    .

--- build/bump.lua
+++ build/bump.lua
@@ -1,19 +1,17 @@
 #!/usr/local/bin/gamecake
 
-do
 
-local args={...}
+args={...}
 
-local vplus=(tonumber(args[1] or 0) or 0)
+vplus=(tonumber(args[1] or 0) or 0)
 
-local wbake=require("wetgenes.bake")
+wbake=require("wetgenes.bake")
 
 version=wbake.version_from_time(os.time(),vplus)
 
-local ss="GAMECAKE_VERSION=\""..version.."\"\n"
+ss="GAMECAKE_VERSION=\""..version.."\"\n"
 wbake.writefile("../exe_gamecake/version.lua",ss)
 
 print(version)
 
-end
 
