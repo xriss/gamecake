6ca9a7ab58e47a974454a91542df9eacaa0647f4
kriss <kriss@xixs.com>
2017-11-05 19:58:33 +0000

    .

--- test/swankyquant/test.lua
+++ test/swankyquant/test.lua
@@ -3,7 +3,7 @@ local grd=require("wetgenes.grd")
 local wstr=require("wetgenes.string")
 
 
-local test=function(fname)
+local test=function(fname,colors)
 
 -- test neoquant as base
 
@@ -26,7 +26,7 @@ local pal_idx={}
 local pal_rgb={}
 local pal_dat={}
 
-for i=0,255 do
+for i=0,colors-1 do
 	pal_rgb[ i*4 + 1 ]=i
 	pal_rgb[ i*4 + 2 ]=i
 	pal_rgb[ i*4 + 3 ]=i
@@ -41,7 +41,11 @@ end
 
 -- assign equal pixels to each bucket as a starting state
 for i=0,go.width*go.height-1 do
-	local p=i%256
+	local r=pix_rgb[ i*4 + 1 ] -- original rgb
+	local g=pix_rgb[ i*4 + 2 ]
+	local b=pix_rgb[ i*4 + 3 ]
+	local a=pix_rgb[ i*4 + 4 ]
+	local p=math.floor(0.5+(colors-1)*((r/255 + g/255 + b/255 )/3))
 	pal_idx[ i + 1 ] = p
 	pal_dat[ p*5 + 1 ]=pal_dat[ p*5 + 1 ] + pix_rgb[ i*4 + 1 ]
 	pal_dat[ p*5 + 2 ]=pal_dat[ p*5 + 2 ] + pix_rgb[ i*4 + 2 ]
@@ -52,14 +56,14 @@ end
 
 -- save current state
 local save=function(idx)
-	for i=0,255 do
+	for i=0,colors-1 do
 		local d=pal_dat[ i*5 + 5 ] ; if d==0 then d=1 end
 		pal_rgb[ i*4 + 1 ]=math.floor(pal_dat[ i*5 + 1 ] / d + 0.5)
 		pal_rgb[ i*4 + 2 ]=math.floor(pal_dat[ i*5 + 2 ] / d + 0.5)
 		pal_rgb[ i*4 + 3 ]=math.floor(pal_dat[ i*5 + 3 ] / d + 0.5)
 		pal_rgb[ i*4 + 4 ]=math.floor(pal_dat[ i*5 + 4 ] / d + 0.5)
 	end
-	go:palette(0,256,pal_rgb)
+	go:palette(0,colors,pal_rgb)
 	go:pixels(0,0,0,go.width,go.height,1,pal_idx)
 	assert( go:save("go/"..fname..".sq."..idx..".png") )
 end
@@ -69,7 +73,7 @@ local cdist=function( ar,ag,ab,aa , br,bg,bb,ba )
 --	ar=255-ar ag=255-ag ab=255-ab aa=255-aa
 --	br=255-br bg=255-bg bb=255-bb ba=255-ba
 --	return (ar*ar-br*br)*(ar*ar-br*br) + (ag*ag-bg*bg)*(ag*ag-bg*bg) + (ab*ab-bb*bb)*(ab*ab-bb*bb) + (aa*aa-ba*ba)*(aa*aa-ba*ba)
-	return (ar-br)*(ar-br) + (ag-bg)*(ag-bg) + (ab-bb)*(ab-bb) + (aa-ba)*(aa-ba)
+	return math.sqrt( (ar-br)*(ar-br) + (ag-bg)*(ag-bg) + (ab-bb)*(ab-bb) + (aa-ba)*(aa-ba) )
 --	return math.abs(ar-br) + math.abs(ag-bg) + math.abs(ab-bb) + math.abs(aa-ba)
 end
 
@@ -92,7 +96,7 @@ local quant=function(w)
 		pal_dat[ p*5 + 5 ]=pal_dat[ p*5 + 5 ] - 1
 
 		local best_d=math.huge
-		for t=0,255 do -- test each bucket
+		for t=0,colors-1 do -- test each bucket
 --			local w=256
 			local d=pal_dat[ t*5 + 5 ]+w ; if d==0 then d=1 end
 			local tr=math.floor( (pal_dat[ t*5 + 1 ]+r*w) / d + 0.5)
@@ -118,19 +122,48 @@ local quant=function(w)
 
 end
 
+-- work out error
+local diff=function()
+	local e,c=0,0
+
+	for i=0,go.width*go.height-1 do
+		local r=pix_rgb[ i*4 + 1 ] -- original rgb
+		local g=pix_rgb[ i*4 + 2 ]
+		local b=pix_rgb[ i*4 + 3 ]
+		local a=pix_rgb[ i*4 + 4 ]
+
+		local p=pal_idx[ i + 1 ] -- original index
+
+		local pr=pix_rgb[ p*4 + 1 ] -- original rgb
+		local pg=pix_rgb[ p*4 + 2 ]
+		local pb=pix_rgb[ p*4 + 3 ]
+		local pa=pix_rgb[ p*4 + 4 ]
+
+		local d=cdist( r,g,b,a , pr,pg,pb,pa )
+		
+		e=e+d
+		c=c+1
+
+	end
+	
+	return e/c
+	
+end
 
 -- loop update and output
 save(0)
 local st=os.time()
 local c=8
 for i=1,c do
-	print(fname,i,os.time()-st)
+	io.flush()
 	quant(256*((c+1-i)/c))
 	save(i)
+	local d=diff()
+	print(fname,i,os.time()-st,d)
 end
 
 end
 
-test("rgb")
-test("baboon")
-test("monarch")
+test("rgb",4)
+test("baboon",4)
+test("monarch",4)
