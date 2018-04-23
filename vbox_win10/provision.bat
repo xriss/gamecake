916e09a519068758a3179ae65d303c0f0e3a9010
kriss <kriss@xixs.com>
2018-04-23 21:13:16 +0100

    .

---
+++ vbox_win10/provision.bat
@@ -0,0 +1,5 @@
+
+choco install -y premake.portable
+
+choco install -y visualstudio2017community --package-parameters "--allWorkloads --includeRecommended --includeOptional --passive --locale en-US"
+
