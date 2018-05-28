70dbfcfd3e9a1176b7a7cdd9fd60a62046fd221f
kriss <kriss@xixs.com>
2018-05-28 02:36:01 +0100

    osx has killed itself, just give up on nfs,

--- vbox_osx/provision.sh
+++ vbox_osx/provision.sh
@@ -10,4 +10,7 @@ export PATH=/opt/local/bin:/usr/local/bin:$PATH
 su vagrant -c " brew install gnu-getopt "
 su vagrant -c " echo 'export PATH=\"/usr/local/opt/gettext/bin:$PATH\"' >> ~/.bash_profile "
 
-su vagrant -c " /gamecake/build/install "
+
+su vagrant -c " cd gamecake && ./git-pull && cd .. || git clone --recursive -v --progress https://github.com/xriss/gamecake.git "
+
+su vagrant -c " gamecake/build/install "
