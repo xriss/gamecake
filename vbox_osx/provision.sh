232d5af8fabc47af4406da51ce1bc68ef9aa03b6
kriss <kriss@xixs.com>
2018-05-28 03:19:01 +0100

    provision and build again

--- vbox_osx/provision.sh
+++ vbox_osx/provision.sh
@@ -7,10 +7,15 @@
 export PATH=/opt/local/bin:/usr/local/bin:$PATH
 
 #must not be root, fix broken osx getopt
+
+su vagrant -c " brew update-reset "
+
 su vagrant -c " brew install gnu-getopt "
-su vagrant -c " echo 'export PATH=\"/usr/local/opt/gettext/bin:$PATH\"' >> ~/.bash_profile "
+su vagrant -c " echo 'export PATH=\"/usr/local/opt/gnu-getopt/bin:$PATH\"' >> ~/.bash_profile "
+
+su vagrant -c " brew install wget "
 
 
 su vagrant -c " cd gamecake && ./git-pull && cd .. || git clone --recursive -v --progress https://github.com/xriss/gamecake.git "
 
-su vagrant -c " gamecake/build/install "
+su vagrant -c " . .bash_profile ; gamecake/build/install "
