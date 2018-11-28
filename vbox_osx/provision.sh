32aa1022f090c7a6e9851c4baa13a853ae246602
kriss <kriss@xixs.com>
2018-11-28 03:07:40 +0000

    bitrot fix

--- vbox_osx/provision.sh
+++ vbox_osx/provision.sh
@@ -11,6 +11,7 @@ export PATH=/opt/local/bin:/usr/local/bin:$PATH
 su vagrant -c " brew update-reset "
 
 su vagrant -c " brew install gnu-getopt "
+su vagrant -c " brew link --force gnu-getopt "
 su vagrant -c " echo 'export PATH=\"/usr/local/opt/gnu-getopt/bin:$PATH\"' >> ~/.bash_profile "
 
 su vagrant -c " brew install wget "
