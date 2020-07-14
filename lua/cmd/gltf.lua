37ce5da2050f24a63ba580a25098d58ee80fa94f
kriss <kriss@xixs.com>
2020-07-14 04:06:37 +0100

    setup a gltf comandline test

---
+++ lua/cmd/gltf.lua
@@ -0,0 +1,67 @@
+
+local wpath=require("wetgenes.path")
+local wstr=require("wetgenes.string")
+local ls=function(s) print(wstr.dump(s)) end
+
+local wgrd=require("wetgenes.grd")
+
+local doublewrap=require("cmd.args").doublewrap
+local splitpath=require("cmd.args").splitpath
+
+local cmds={
+	{ "info",		"Display information about a gltf file."},
+}
+for i,v in ipairs(cmds) do
+	v.name=v[1]
+	v.help=v[2]
+end
+table.sort(cmds,function(a,b) return a.name<b.name end)
+
+
+-- generic options for all commands
+local default_inputs=function(inputs)
+	for i,v in ipairs{
+
+		{	"help",			false,	"Print help and exit.", },
+
+	} do
+		inputs[#inputs+1]=v
+	end
+	return inputs
+end
+
+local cmd=table.remove(arg,1)       -- check what cmd is asked for
+local cmd=cmd and string.lower(cmd) -- force lowercase
+
+if cmd=="info" then
+
+	local args=require("cmd.args").bake({inputs=default_inputs{
+
+	}}):parse(arg):sanity()
+	
+	if args.data.help or not args.data[1] then
+		print("\n"..arg[0].." info --OPTIONS input.gltf \n")
+		print( "where --OPTIONS is any combination of the following :\n")
+		print(table.concat(args:help(),"\n"))
+		print("")
+		os.exit(0)
+	end
+	
+	for i,v in ipairs(args.data) do
+
+		local path=splitpath( args.data[i] )
+		path.idx=string.format("%03d",i)
+		local input=path.path
+
+		print( input.." -> "..output )
+
+	end
+
+else -- print help
+
+	print("\n"..arg[0].." COMMAND ".."...\n")
+	print("Where COMMAND is one of the following :\n")
+	print("  "..table.concat(doublewrap(cmds,78,30," : "),"\n  ").."\n")
+	os.exit(0)
+
+end
