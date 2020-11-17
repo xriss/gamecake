e009ed454ff9cdec95b65dc0ea20558a066d2db7
kriss <kriss@xixs.com>
2020-11-17 19:33:47 +0000

    test

---
+++ lua/wetgenes/txt/ipa_symbols.lua
@@ -0,0 +1,76 @@
+
+local ipa_ascii_map={
+
+	["a:"]		=	{	"a0",	"a",	},	--		ɑ:			arm			father
+	["@"]		=	{	"a1",	"A",	},	--		æ			cat			black
+	["ei"]		=	{	"a2",			},	--		eɪ			say			eight
+	[".."]		=	{	"a3",			},	--		ə			away		cinema
+
+	["b"]		=	{	"b0",	"b",	},	--		 b			bad			lab
+
+	["tS"]		=	{	"c0",	"c",	},	--		tʃ			check		church
+
+	["d"]		=	{	"d0",	"d",	},	--		d			did			lady
+	["TH"]		=	{	"d1",	"D",	},	--		ð			this		mother
+
+	["e"]		=	{	"e0",	"e",	},	--		e			met			bed
+	["e..(r)"]	=	{	"e1",	"E",	},	--		eəʳ			where		air
+	["i..(r)"]	=	{	"e2",			},	--		ɪəʳ			near		here
+
+	["f"]		=	{	"f0",	"f",	},	--		f			find		if
+	["th"]		=	{	"f1",	"F",	},	--		θ			think		both
+
+	["g"]		=	{	"g0",	"g",	},	--		g			give		flag
+
+	["h"]		=	{	"h0",	"h",	},	--		h			how			hello
+
+	["i"]		=	{	"i0",	"i",	},	--		ɪ			hit			sitting
+	["i:"]		=	{	"i1",	"I",	},	--		i:			see			heat
+	["ai"]		=	{	"i2",			},	--		aɪ			five		eye
+
+	["dZ"]		=	{	"j0",	"j",	},	--		dʒ			just		large
+							
+	["k"]		=	{	"k0",	"k",	},	--		k			cat			back
+
+	["l"]		=	{	"l0",	"l",	},	--		l			leg			little
+
+	["m"]		=	{	"m0",	"m",	},	--		m			man			lemon
+
+	["n"]		=	{	"n0",	"n",	},	--		n			no			ten
+	["N"]		=	{	"n1",	"N",	},	--		ŋ			sing		finger
+
+	["o"]		=	{	"o0",	"o",	},	--		ɒ			hot			rock
+	["o:"]		=	{	"o1",	"O",	},	--		ɔ:			call		four
+	["Ou"]		=	{	"o2",			},	--		oʊ			go			home
+	["au"]		=	{	"o3",			},	--		aʊ			now			out
+	["oi"]		=	{	"o4",			},	--		ɔɪ			boy			join
+
+	["p"]		=	{	"p0",	"p",	},	--		p			pet			map
+
+--								"q"
+
+	["r"]		=	{	"r0",	"r",	},	--		r			red			try
+
+	["s"]		=	{	"s0",	"s",	},	--		s			sun			miss
+	["S"]		=	{	"s1",	"S",	},	--		ʃ			she			crash
+
+	["t"]		=	{	"t0",	"t",	},	--		t			tea			getting
+
+	["u"]		=	{	"u0",	"u",	},	--		ʊ			put			could
+	["^"]		=	{	"u1",	"U",	},	--		ʌ			cup			luck
+	["u..(r)"]	=	{	"u2",			},	--		ʊəʳ			pure		tourist
+	["u:"]		=	{	"u3",			},	--		u:			blue		food
+	["e:(r)"]	=	{	"u4",			},	--		ɜ:ʳ			turn		learn
+
+	["v"]		=	{	"v0",	"v",	},	--		v			voice		five
+
+	["w"]		=	{	"w0",	"w",	},	--		w			wet			window
+
+--								"x"
+
+	["j"]		=	{	"y0",	"y",	},	--		j			yes			yellow
+							
+	["z"]		=	{	"z0",	"z",	},	--		z			zoo			lazy
+	["Z"]		=	{	"z1",	"Z",	},	--		ʒ			pleasure	vision
+
+}
