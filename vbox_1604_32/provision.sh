3a2c8fa043c39706efb81c0dfbe3d56c03a25738
xriss <kriss@xixs.com>
2021-09-04 00:16:03 +0100

    luajit seems borked on the 16.04 32bit vbox? so use lua

--- vbox_1604_32/provision.sh
+++ vbox_1604_32/provision.sh
@@ -2,6 +2,9 @@
 # update and install some basic dependencies
 
 sudo apt-get update
+sudo apt-get upgrade
 
 /gamecake/build/install
 
+sudo apt-get install -y lua5.1
+sudo apt-get install -y lua-filesystem
