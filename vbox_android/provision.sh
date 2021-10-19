c53b166e16699facf6aeb4f13146a8a024873eb3
xriss <kriss@xixs.com>
2021-10-19 02:49:16 +0100

    I think we are now building 4 android cpu versions of luajit and sdl2 OK

--- vbox_android/provision.sh
+++ vbox_android/provision.sh
@@ -5,5 +5,7 @@ sudo apt -y update
 sudo apt -y upgrade
 
 #must not be root?
-su vagrant -c /gamecake/build/install-andsdk /home/vagrant/
+su vagrant -c "/gamecake/build/install-andsdk /home/vagrant/"
+
+su vagrant -c "source /home/vagrant/andsdk-env.sh && /gamecake/build/install --android"
 
