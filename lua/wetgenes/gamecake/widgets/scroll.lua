b84ce5b5ff63b6077f6500b494d28747ab49d27f
kriss <kriss@xixs.com>
2022-04-06 00:11:29 +0100

    fix snap when scrolling

--- lua/wetgenes/gamecake/widgets/scroll.lua
+++ lua/wetgenes/gamecake/widgets/scroll.lua
@@ -79,14 +79,14 @@ function wscroll.layout(widget)
 	widget.pan:layout() -- creates hx_max,hy_max
 
 --	widget.datx.step= widget:bubble("text_size") or 16
-	widget.datx.max=widget.pan.hx_max-widget.pan.hx
-	if widget.datx.max<0 then widget.datx.max=0 else widget.datx.max=widget.datx.max+widget.datx.step end
+	widget.datx.max=math.ceil((widget.pan.hx_max-widget.pan.hx)/widget.datx.step)*widget.datx.step
+	if widget.datx.max<0 then widget.datx.max=0  end
 	widget.datx.size=widget.pan.hx/widget.pan.hx_max
 	widget.datx:value() -- clamp
 	
 --	widget.daty.step= widget:bubble("text_size") or 16
-	widget.daty.max=1+widget.pan.hy_max-widget.pan.hy
-	if widget.daty.max<0 then widget.daty.max=0 else widget.daty.max=widget.daty.max+widget.daty.step end
+	widget.daty.max=math.ceil((1+widget.pan.hy_max-widget.pan.hy)/widget.daty.step)*widget.daty.step
+	if widget.daty.max<0 then widget.daty.max=0 end
 	widget.daty.size=widget.pan.hy/widget.pan.hy_max
 	widget.daty:value() -- clamp
 	
