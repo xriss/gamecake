8e6ca37e64548f2e86bf0d2d86f4ea8639b5333c
kriss <kriss@xixs.com>
2022-08-30 18:33:27 +0100

    osx hax

--- vbox_osx/provision.sh
+++ vbox_osx/provision.sh
@@ -1,27 +1,20 @@
 
-# update and install some basic dependencies
-
-#xcode-select --install
-
-
-export PATH=/opt/local/bin:/usr/local/bin:$PATH
-
-#must not be root, fix broken osx getopt
-
-su vagrant -c " brew update-reset "
-
-su vagrant -c " brew install gnu-getopt "
-su vagrant -c " brew link --force gnu-getopt "
-su vagrant -c " echo 'export PATH=\"/usr/local/opt/gnu-getopt/bin:$PATH\"' >> ~/.bash_profile "
-
-su vagrant -c " brew install wget "
-
-su vagrant -c " brew install luarocks "
-su vagrant -c " luarocks install luafilesystem "
+echo " updating brew "
+su vagrant -c " brew update "
+su vagrant -c " git -C /usr/local/Homebrew/Library/Taps/homebrew/homebrew-core fetch --unshallow "
+su vagrant -c " brew update --auto-update "
 
 
-su vagrant -c " cd gamecake && ./git-pull && cd .. || git clone --recursive -v --progress https://github.com/xriss/gamecake.git "
+echo " installing bash "
+su vagrant -c " brew install bash "
+echo /usr/local/bin/bash | sudo tee -a /private/etc/shells
+sudo chpass -s /usr/local/bin/bash vagrant
 
-su vagrant -c " . .bash_profile ; gamecake/build/install "
 
+echo " fetching gamecake "
+su vagrant -c " cd gamecake && ./git-pull && cd .. || git clone --recursive -v --progress https://github.com/xriss/gamecake.git  "
+
+
+#echo " installing gamecake dependencies "
+#su vagrant -c " /usr/local/bin/bash -c \"  gamecake/build/install  \" "
  
--- vbox_osx/readme.md
+++ vbox_osx/readme.md
@@ -1,20 +1,5 @@
 This is partially working again...
 
-Auto key setup seems to fail so, need to enable dss in ,ssh/config
 
-	Host *
-		HostkeyAlgorithms +ssh-dss
-		PubkeyAcceptedKeyTypes +ssh-dss
-
-and then
-
-	vagrant up
-
-	ssh-copy-id ssh://vagrant@127.0.0.1:2222
-
-
-then we can use ssh to autologin
-
-	ssh ssh://vagrant@127.0.0.1:2222
 
 
