a2d5532abb995f96eae2636b058b8356bec11e5d
kriss <kriss@xixs.com>
2023-06-01 03:46:32 +0100

    modern words list

--- lua/wetgenes/txt/words/download.sh
+++ lua/wetgenes/txt/words/download.sh
@@ -2,4 +2,6 @@
 
 #curl https://raw.githubusercontent.com/xriss/engrish/main/chambers/words.tsv -o eng.tsv
 
-curl https://raw.githubusercontent.com/xriss/engrish/main/all/words.tsv -o eng.tsv
+#curl https://raw.githubusercontent.com/xriss/engrish/main/all/words.tsv -o eng.tsv
+
+curl https://raw.githubusercontent.com/xriss/engrish/main/wordnet/words.tsv -o eng.tsv
