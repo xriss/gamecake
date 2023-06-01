1a12a4cbe84bc3901efd9972c16cc07049a8b5a3
kriss <kriss@xixs.com>
2023-06-01 02:48:06 +0100

    more words

--- lua/wetgenes/txt/words/download.sh
+++ lua/wetgenes/txt/words/download.sh
@@ -1,5 +1,5 @@
 
 
-curl https://raw.githubusercontent.com/xriss/engrish/main/chambers/words.tsv -o eng.tsv
+#curl https://raw.githubusercontent.com/xriss/engrish/main/chambers/words.tsv -o eng.tsv
 
-
+curl https://raw.githubusercontent.com/xriss/engrish/main/all/words.tsv -o eng.tsv
