3aaaad0107edb07dee5d6fc5d5d069a82a18ed0d
kriss <kriss@xixs.com>
2023-06-15 02:01:31 +0100

    now I have a plan

---
+++ flatpack/download.sh
@@ -0,0 +1,7 @@
+
+#TODO
+
+echo need to parse this, extract the url for multiple .snap files unpack them using squashfs and then we have a multiple binary builds we can publish in a flatpack
+
+curl -H 'Snap-Device-Series: 16' http://api.snapcraft.io/v2/snaps/info/gamecake
+
