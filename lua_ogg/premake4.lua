6f94507f45939c9e95c3433a5d2cc2d89d73730c
kriss <kriss@xixs.com>
2023-07-11 03:36:02 +0100

    fix relative includes

--- lua_ogg/premake4.lua
+++ lua_ogg/premake4.lua
@@ -5,7 +5,7 @@ files { "code/**.cpp" , "code/**.c" , "code/**.h" , "all.h" }
 
 links { "lib_lua" }
 
-includedirs { "." , "../lib_vorbis/include" , "../lib_ogg/include" }
+includedirs { "." , "../lib_vorbis/vorbis/include" , "../lib_ogg/ogg/include" }
 
 KIND{lua="wetgenes.ogg.core"}
 
