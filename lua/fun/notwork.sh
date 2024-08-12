c44115df26c845987d92661a7bf1c5cba1c59b5c
kriss <kriss@xixs.com>
2024-08-12 12:17:52 +0100

    auto size

--- lua/fun/notwork.sh
+++ lua/fun/notwork.sh
@@ -2,8 +2,11 @@ BASEDIR=$( cd "$( dirname "$0" )" && pwd )
 
 cd $BASEDIR/../..
 
-exe/gamecake lua/fun/notwork.fun.lua --win-hx=800 --win-hy=450 --win-px=800 --win-py=0   --win-borderless --quickexit --host=2342 &\
-sleep 1 ; exe/gamecake lua/fun/notwork.fun.lua --win-hx=800 --win-hy=450 --win-px=800 --win-py=450 --win-borderless --quickexit --host=2342 --join=[::1]:2342 &\
+HX=$( xdpyinfo | grep dimensions: | awk '{print $2}' | cut -d 'x' -f1 )
+HY=$( xdpyinfo | grep dimensions: | awk '{print $2}' | cut -d 'x' -f2 )
+
+          exe/gamecake lua/fun/notwork.fun.lua --win-hx=$((HX/2)) --win-hy=$((HY/2)) --win-px=$((HX/2)) --win-py=0         --win-borderless --quickexit --host=2342 &\
+sleep 1 ; exe/gamecake lua/fun/notwork.fun.lua --win-hx=$((HX/2)) --win-hy=$((HY/2)) --win-px=$((HX/2)) --win-py=$((HY/2)) --win-borderless --quickexit --host=2342 --join=[::1]:2342 &\
 wait
 
 
