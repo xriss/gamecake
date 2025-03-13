42bfaded4aee03bfc4d6f5e484b0036b91b1b3a8
kriss <kriss@xixs.com>
2025-03-13 14:48:24 +0000

    use slighty older debian build as a stable point

--- vbox_x64/provision.sh
+++ vbox_x64/provision.sh
@@ -6,8 +6,8 @@ sudo apt-get update
 sudo /gamecake/install-build-dependencies
 sudo /gamecake/build/install
 
-sudo apt-get -y install clang-19
-sudo apt-get -y remove clang
-sudo ln -s /usr/bin/clang-19 /usr/bin/clang
-sudo ln -s /usr/bin/clang++-19 /usr/bin/clang++
-sudo ln -s /usr/bin/clang-cpp-19 /usr/bin/clang-cpp
+#sudo apt-get -y install clang-19
+#sudo apt-get -y remove clang
+#sudo ln -s /usr/bin/clang-19 /usr/bin/clang
+#sudo ln -s /usr/bin/clang++-19 /usr/bin/clang++
+#sudo ln -s /usr/bin/clang-cpp-19 /usr/bin/clang-cpp
