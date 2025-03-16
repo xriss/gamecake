caad145483d861227cc3c0fcf0e8320947e12c63
kriss <kriss@xixs.com>
2025-03-16 12:03:58 +0000

    update amalgamated license

--- build/licenses.lua
+++ build/licenses.lua
@@ -2,33 +2,65 @@
 
 local lcs={
 
-	["lib_chipmonk"]	="lib_chipmonk/LICENSE.txt",
 	["lib_freetype"]	="lib_freetype/freetype/docs/LICENSE.TXT",
-	["lib_gif"]			="lib_gif/COPYING",
-	["lib_hidapi"]		="lib_hidapi/LICENSE-bsd.txt",
-	["lib_jpeg"]		="lib_jpeg/README",
+	["lib_gif"]			="lib_gif/giflib/COPYING",
+	["lib_jpeg"]		="lib_jpeg/jpeg/README",
 	["lib_lua"]			="lib_lua/COPYRIGHT",
-	["lib_luajit"]		="lib_luajit/COPYRIGHT",
-	["lib_ogg"]			="lib_ogg/COPYING",
-	["lib_openal"]		="lib_openal/asoft/COPYING",
-	["lib_openssl"]		="lib_openssl/LICENSE",
+	["lib_ogg"]			="lib_ogg/ogg/COPYING",
+	["lib_openal"]		="lib_openal/mojoal/LICENSE.txt",
+	["lib_opus"]		="lib_opus/opus/COPYING",
 	["lib_pcre"]		="lib_pcre/LICENCE",
-	["lib_png"]			="lib_png/LICENSE",
-	["lib_vorbis"]		="lib_vorbis/COPYING",
-	["lib_vpx"]			="lib_vpx/git/LICENSE",
+	["lib_png"]			="lib_png/fixed/LICENSE",
+	["lib_pq"]			="lib_pq/COPYRIGHT",
+	["lib_speexdsp"]	="lib_speexdsp/speexdsp/COPYING",
+--	["lib_sqlite"]		=PUBLIC_DOMAIN,
+	["lib_vorbis"]		="lib_vorbis/vorbis/COPYING",
+	["lib_wolfssl"]		="lib_wolfssl/LICENSE",
 	["lib_z"]			="lib_z/README",
+	["lib_zip"]			="lib_zip/LICENSE",
 	["lib_zzip"]		="lib_zzip/COPYING.LIB",
 
+--	["lua_al"]			=GAMECAKE_LICENSE,
 	["lua_bit"]			="lua_bit/README",
-	["lua_lanes"]		="lua_lanes/COPYRIGHT",
+	["lua_brimworkszip"]="lua_brimworkszip/README",
+	["lua_bullet"]		="lua_bullet/LICENSE.txt",
+	["lua_chipmunk"]	="lua_chipmunk/master/LICENSE.txt",
+	["lua_cmsgpack"]	="lua_cmsgpack/README.md",
+	["lua_djon"]		="lua_djon/git/LICENSE.md",
+--	["lua_fats"]		=GAMECAKE_LICENSE,
+--	["lua_freetype"]	=GAMECAKE_LICENSE,
+--	["lua_gamecake"]	=GAMECAKE_LICENSE,
+--	["lua_gles"]		=GAMECAKE_LICENSE,
+	["lua_glslang"]		="lua_glslang/glslang/LICENSE.txt",
+--	["lua_grd"]			=GAMECAKE_LICENSE,
+--	["lua_grdmap"]		=GAMECAKE_LICENSE,
+	["lua_kissfft"]		="lua_kissfft/kissfft/COPYING",
+	["lua_lanes"]		="lua_lanes/lanes/COPYRIGHT",
 	["lua_lash"]		="lua_lash/src/LICENSE",
 	["lua_lfs"]			="lua_lfs/LICENSE",
+	["lua_linenoise"]	="lua_linenoise/COPYING",
+--	["lua_midi"]		=GAMECAKE_LICENSE,
+--	["lua_ogg"]			=GAMECAKE_LICENSE,
+--	["lua_opus"]		=GAMECAKE_LICENSE,
+--	["lua_pack"]		=GAMECAKE_LICENSE,
+	["lua_pegasus"]		="lua_pegasus/LICENSE",
+	["lua_periphery"]	="lua_periphery/LICENSE",
+	["lua_pgsql"]		="lua_pgsql/README",
 	["lua_posix"]		="lua_posix/COPYING",
-	["lua_profiler"]	="lua_profiler/LICENSE",
+	["lua_rex"]			="lua_rex/LICENSE",
+	["lua_sdl2"]		="lua_sdl2/luasdl2/LICENSE",
 	["lua_sec"]			="lua_sec/LICENSE",
 	["lua_socket"]		="lua_socket/LICENSE",
+--	["lua_sod"]			=GAMECAKE_LICENSE,
+--	["lua_sqlite"]		=MIT_LICENSE",
+	["lua_sys"]			="lua_sys/luasys/COPYRIGHT",
+--	["lua_tardis"]		=GAMECAKE_LICENSE,
+--	["lua_utf8"]		=LUA_LICENSE,
+--	["lua_v4l2"]		=GAMECAKE_LICENSE,
+--	["lua_win"]			=GAMECAKE_LICENSE,
+--	["lua_win_emcc"]	=GAMECAKE_LICENSE,
 	["lua_zip"]			="lua_zip/LICENSE",
-	["lua_zlib"]		="lua_zip/README",
+	["lua_zlib"]		="lua_zlib/src/README",
 
 }
 
@@ -36,7 +68,6 @@ local wbake=require("wetgenes.bake")
 
 local head=[[
 Files: *
-Copyright: 2013 Kriss Blank <kriss@wetgenes.com>
 License: The MIT License (MIT)
  Copyright (c) 2013 Kriss Blank <kriss@wetgenes.com>
  
@@ -69,7 +100,6 @@ for n,v in pairs(lcs) do
 	local t=" "..string.gsub(d,"\n","\n ")
 	
 	print("Files: "..n.."/*")
-	print("Copyright: ...")
 	print("License: ...")
 	print(t.."\n")
 
