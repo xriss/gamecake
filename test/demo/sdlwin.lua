84daa882c11a03805b96a0a944d3a4509f7e437d
kriss <kriss@xixs.com>
2025-03-20 10:19:09 +0000

    .

--- test/demo/sdlwin.lua
+++ test/demo/sdlwin.lua
@@ -4,11 +4,13 @@
 
 local SDL	= require "SDL"
 
+print("SDL.init",SDL.flags.Video)
 local ret, err = SDL.init { SDL.flags.Video }
 if not ret then
 	error(err)
 end
 
+print("SDL.createWindow", SDL.window.Resizable , SDL.flags.OpenGL )
 local win = assert( SDL.createWindow {
 	title	= "02 - Opening a window",	-- optional
 	width	= 320,				-- optional
@@ -16,11 +18,13 @@ local win = assert( SDL.createWindow {
 	flags	= { SDL.window.Resizable , SDL.flags.OpenGL }	-- optional
 } )
 
+print("win:setFullscreen")
 win:setFullscreen(SDL.window.Desktop)
 --win:setFullscreen(SDL.window.Fullscreen)
 
 
 
+print("SDL.createRenderer")
 local rdr = assert( SDL.createRenderer( win, -1) )
 
 	rdr:setDrawColor(0xFF0000)
@@ -29,6 +33,7 @@ local rdr = assert( SDL.createRenderer( win, -1) )
 	
 	
 -- Let the window opened a bit
+print("SDL.Delay",5000)
 SDL.delay(5000)
 
 
