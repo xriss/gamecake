a7a93a6c8f71fe6100ff0fe1b3d01be39cdda73d
kriss <kriss@xixs.com>
2025-10-14 02:01:56 +0100

    tasks should auto update the simpler syncs so oven willl make sure that syncs are run sometimes

--- lua/wetgenes/tasks.lua
+++ lua/wetgenes/tasks.lua
@@ -9,6 +9,9 @@ do
 local coroutine,package,string,table,math,io,os,debug,assert,dofile,error,_G,getfenv,getmetatable,ipairs,Gload,loadfile,loadstring,next,pairs,pcall,print,rawequal,rawget,rawset,select,setfenv,setmetatable,tonumber,tostring,type,unpack,_VERSION,xpcall,module,require
      =coroutine,package,string,table,math,io,os,debug,assert,dofile,error,_G,getfenv,getmetatable,ipairs, load,loadfile,loadstring,next,pairs,pcall,print,rawequal,rawget,rawset,select,setfenv,setmetatable,tonumber,tostring,type,unpack,_VERSION,xpcall,module,require
 
+
+local wsyncs=require("wetgenes.syncs")
+
 -- module
 
 M.tasks={}
@@ -520,6 +523,7 @@ M.tasks.memos=function(tasks,subid)
 end
 
 M.tasks.update=function(tasks)
+	wsyncs.run()
 end
 
 --[[#lua.wetgenes.tasks.create
