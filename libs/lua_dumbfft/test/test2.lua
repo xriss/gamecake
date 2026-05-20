dc3d658b98b897b4fbd01265412a5b37582e7f03
kriss <kriss@xixs.com>
2026-05-21 00:50:33 +0100

    .

--- libs/lua_dumbfft/test/test2.lua
+++ libs/lua_dumbfft/test/test2.lua
@@ -2,45 +2,45 @@
 -- expect this many s16 audio samples per second
 local sample_rate=48000
 
-local note_freq_octs={
-{ [4]=261.63 },
-{ [4]=277.18 },
-{ [4]=293.66 },
-{ [4]=311.13 },
-{ [4]=329.63 },
-{ [4]=349.23 },
-{ [4]=369.99 },
-{ [4]=392.00 },
-{ [4]=415.30 },
-{ [4]=440.00 },
-{ [4]=466.16 },
-{ [4]=493.88 },
+local note_wavelen_octs={
+{ [4]=1/261.63 },
+{ [4]=1/277.18 },
+{ [4]=1/293.66 },
+{ [4]=1/311.13 },
+{ [4]=1/329.63 },
+{ [4]=1/349.23 },
+{ [4]=1/369.99 },
+{ [4]=1/392.00 },
+{ [4]=1/415.30 },
+{ [4]=1/440.00 },
+{ [4]=1/466.16 },
+{ [4]=1/493.88 },
 }
-for i,v in ipairs(note_freq_octs) do -- build other octaves
-	v[-1]=v[4]/32
-	v[0]=v[4]/16
-	v[1]=v[4]/8
-	v[2]=v[4]/4
-	v[3]=v[4]/2
+for i,v in ipairs(note_wavelen_octs) do -- build other octaves
+	v[-1]=v[4]*32
+	v[0]=v[4]*16
+	v[1]=v[4]*8
+	v[2]=v[4]*4
+	v[3]=v[4]*2
 	--
-	v[5]=v[4]*2
-	v[6]=v[4]*4
-	v[7]=v[4]*8
-	v[8]=v[4]*16
-	v[9]=v[4]*32
+	v[5]=v[4]/2
+	v[6]=v[4]/4
+	v[7]=v[4]/8
+	v[8]=v[4]/16
+	v[9]=v[4]/32
 end
 
-local midi_freqs={} -- 0-127 but 
+local midi_wavelen={} -- 0-127 but 
 for o=-1,9 do
 	for n=1,12 do -- build other octaves
-		local f=note_freq_octs[n][o]
+		local f=note_wavelen_octs[n][o]
 		local m=((o+1)*12)+n-1
-		midi_freqs[m]=f
+		midi_wavelen[m]=f
 	end
 end
 
 for i=0,127 do
-	print( midi_freqs[i] , math.ceil(sample_rate/midi_freqs[i]) )
+	print( midi_wavelen[i] , math.ceil(sample_rate*midi_wavelen[i]) )
 end
 
 -- the midi notes we plan to bucket
@@ -55,9 +55,9 @@ local new_bucket=function(midi,size)
 	local bucket={}
 	
 	bucket.midi=midi
-	bucket.hz=midi_freqs[midi]
+	bucket.wavelen=midi_wavelen[midi]
 	bucket.size=size -- fixed size giving us a time sample resolution
-	bucket.probe_size=math.floor(0.5+(sample_rate/bucket.hz))
+	bucket.probe_size=math.floor(0.5+(sample_rate*bucket.wavelen))
 	bucket.probe_cos=math.ceil(bucket.probe_size/4)
 	bucket.probe_idx=0
 	bucket.probe_data={}
@@ -68,6 +68,8 @@ local new_bucket=function(midi,size)
 	bucket.sdata={}
 	bucket.cdata={}
 	
+--	bucket.size=math.ceil(bucket.size/bucket.probe_size)*bucket.probe_size
+
 	bucket.wave=function(t)
 		return math.sin(math.pi*2*t)
 	end
@@ -97,8 +99,7 @@ local new_bucket=function(midi,size)
 		bucket.idx=(bucket.idx+1)%bucket.size -- advance idx along rotational buffer
 	end
 	bucket.get=function()
-		return ( math.abs(bucket.stotal/bucket.size) + math.abs(bucket.ctotal/bucket.size) )*(bucket.hz/sample_rate)
---		return math.sqrt( (bucket.stotal*bucket.stotal) + (bucket.ctotal*bucket.ctotal) )*(bucket.hz/sample_rate)/bucket.size
+		return ( math.abs(bucket.stotal/bucket.size) + math.abs(bucket.ctotal/bucket.size) ) * bucket.wavelen
 	end
 	
 	return bucket
@@ -124,12 +125,12 @@ local print_buckets=function()
 		if num>100 then num=100 end
 		num=math.floor(num)
 		tots=tots+num
-		print(m,math.floor(midi_freqs[m]),num,string.rep("#",num))
+		print(m,math.floor(1/midi_wavelen[m]),num,string.rep("#",num))
 	end
 	print("...",tots)
 end
 
-for f=200,500,100 do
+for f=100,500,100 do
 
 for i=1,4096 do 
 	local w=math.sin( ( f*i/sample_rate )*math.pi*2 )
