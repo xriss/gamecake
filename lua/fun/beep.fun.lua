81e2c6a7c66b01defae692ead8cf9d672a314916
kriss <kriss@xixs.com>
2026-07-04 12:36:55 +0100

    make synth functions thread safe

--- lua/fun/beep.fun.lua
+++ lua/fun/beep.fun.lua
@@ -1,6 +1,405 @@
 --
 -- This is fun64 code, you can copy paste it into https://xriss.github.io/fun64/pad/ to run it.
 --
+
+-- be careful not to include exploding locals as these small functions will get
+-- moved to another thread for rendering and try and take everything with them
+--local note2freq = require("wetgenes.gamecake.fun.bitsynth").note2freq
+--local fwav = require("wetgenes.gamecake.fun.bitsynth").fwav
+
+local midinote_to_freq=function(m)
+	return (2^((m-69)/12))*440
+end
+local note_to_freq=function(s)
+	local lookup={ ["C"]=0,["C#"]=1,["D"]=2,["D#"]=3,["E"]=4,["F"]=5,["F#"]=6,["G"]=7,["G#"]=8,["A"]=9,["A#"]=10,["B"]=11}
+	local o=tonumber(s:sub(-1)) or 0
+	local n=lookup[ s:sub(1,-2):upper() ] or 0
+	return midinote_to_freq( (o+2)*12 + (n-1) )
+end
+
+beeps={}
+beeps["1"]={
+fwav="sine",
+duty=0.5,
+adsr={
+	1,
+	0,0,0.4,0.1
+},
+}
+
+beeps["2"]={
+fwav="triangle",
+adsr={
+	1,
+	0,0,0.4,0.1
+},
+}
+
+beeps["3"]={
+fwav="sawtooth",
+duty=0.125,
+adsr={
+	1,
+	0,0,0.4,0.1
+},
+}
+
+beeps["4"]={
+fwav="toothsaw",
+duty=0.125,
+adsr={
+	1,
+	0,0,0.4,0.1
+},
+}
+
+beeps["5"]={
+fwav="square",
+duty=0.5,
+adsr={
+	1,
+	0,0,0.4,0.1
+},
+}
+
+beeps["6"]={
+fwav="whitenoise",
+frequency="C2",
+adsr={
+	1,
+	0,0,0.4,0.1
+},
+}
+beeps["7"]={
+fwav="whitenoise",
+frequency="C4",
+adsr={
+	1,
+	0,0,0.4,0.1
+},
+}
+beeps["8"]={
+fwav="whitenoise",
+frequency="C6",
+adsr={
+	1,
+	0,0,0.4,0.1
+},
+}
+
+beeps["0"]={
+fwav=function(t)
+	local t2=t/8
+	local n=math.sin( (t%1) * math.pi*2  ) * math.sin( ((t2)%1) * math.pi*2  )
+	return n
+end,
+adsr={
+	1,
+	0,0,0.4,0.1
+},
+}
+
+beeps["q"]={
+fwav="sine",
+adsr={
+	0.5,
+	0.0, 0.0, 2.0, 0.1
+},
+fm={
+	frequency=8,
+--	    fwav="toothsaw",
+	fwav="sawtooth",
+--	    fwav="square",
+	ffreq=function(it)
+	local f1=note_to_freq("C4")
+	local f2=note_to_freq("D4")
+	local f3=note_to_freq("E4")
+	local t1=0
+	local f=function(m,t)
+		if m<0 then return f2+((f1-f2)*-m)+t*t1 end
+		return f2+((f3-f2)*m)+t*t1
+	end
+	return f
+	end,
+}
+}
+
+--[[
+beeps["w"]={
+fwav="sine",
+adsr={
+	1,
+	0,0,1,0.1
+},
+fm={
+	frequency=1024,
+	fwav="sine",
+	ffreq=function(it)
+	local f1=note2freq("C3")
+	local f2=note2freq("D3")
+	local f3=note2freq("E3")
+	local t1=64
+	local f=function(m,t)
+		if m<0 then return f2+((f1-f2)*-m)+fwav.square(t*4)*t1 end
+		return f2+((f3-f2)*m)+fwav.square(t*4)*t1
+	end
+	return f
+	end,
+}
+}
+]]
+
+beeps["e"]={
+fwav="sine",
+adsr={
+	1,
+	0,0,0.4,0.1
+},
+fm={
+	frequency=16,
+	fwav="sawtooth",
+	ffreq=function(it)
+	local f1=note_to_freq("C3")
+	local f2=note_to_freq("C4")
+	local f3=note_to_freq("C5")
+	local t1=0
+	local f=function(m,t)
+		if m<0 then return f2+((f1-f2)*-m)+t*t1 end
+		return f2+((f3-f2)*m)+t*t1
+	end
+	return f
+	end,
+}
+}
+
+beeps["r"]={
+fwav="sine",
+adsr={
+	1,
+	0,0,0.4,0.1
+},
+fm={
+	frequency=16,
+	fwav="triangle",
+	ffreq=function(it)
+	local f1=note_to_freq("C3")
+	local f2=note_to_freq("C4")
+	local f3=note_to_freq("C5")
+	local t1=0
+	local f=function(m,t)
+		if m<0 then return f2+((f1-f2)*-m)+t*t1 end
+		return f2+((f3-f2)*m)+t*t1
+	end
+	return f
+	end,
+}
+}
+
+beeps["t"]={
+fwav="sawtooth",
+adsr={
+	1,
+	0,0,0.4,0.5
+},
+fm={
+	frequency=128,
+	fwav="square",
+	ffreq=function(it)
+	local f1=note_to_freq("C3")
+	local f2=note_to_freq("C4")
+	local f3=note_to_freq("C5")
+	local t1=0
+	local f=function(m,t)
+		if m<0 then return f2+((f1-f2)*-m)+t*t1 end
+		return f2+((f3-f2)*m)+t*t1
+	end
+	return f
+	end,
+}
+}
+
+beeps["a"]={
+fwav="sawtooth",
+adsr={
+	0.75,
+	0.0,0.1,0.4,0.3
+},
+fm={
+	frequency=16,
+	fwav="sine",
+	duty=0.5,
+	ffreq=function(it)
+	local f1=note_to_freq("C3")
+	local f2=note_to_freq("D3")
+	local f3=note_to_freq("E3")
+	local t1=0
+	local f=function(m,t)
+--			return f2+t*t1
+		local t2=math.pow(t+2,4)*t1
+		if m<0 then return f2+((f1-f2)*-m)+t2 end
+		return f2+((f3-f2)*m)+t2
+	end
+	return f
+	end,
+},
+fread=function(it)
+	return function(t)
+	it.fm_gwav.set_frequency(2*(t+1))
+	end
+end
+}
+
+beeps["z"]={
+fwav="triangle",
+frequency="D4",
+duty=0.25,
+adsr={
+	0.7,
+	0.2,0.2,0.8,0.2
+},
+--[[
+fm={
+	frequency=16,
+	fwav="sine",
+	duty=0.5,
+	ffreq=function(it)
+	local f1=bitsynth.C3
+	local f2=bitsynth.D3
+	local f3=bitsynth.E3
+	local t1=0
+	local f=function(m,t)
+--			return f2+t*t1
+--			local t2=math.pow(t+2,4)*t1
+--			if m<0 then return f2+((f1-f2)*-m)+t2 end
+--			return f2+((f3-f2)*m)+t2
+		return bitsynth.D3
+	end
+	return f
+	end,
+},
+]]
+--	fread=function(it)
+--	    return function(t)
+--		it.fm_gwav.set_frequency(2*(t+1))
+--	    end
+--	end
+}
+
+
+beeps["x"]={
+fwav="sine",
+frequency="C5",
+volume=1.0,
+duty=0.5,
+adsr={
+	0.7,
+	0.2,0.2,0.8,0.2
+},
+}
+beeps["c"]={
+fwav="triangle",
+frequency="C5",
+volume=1.0,
+duty=0.5,
+adsr={
+	0.7,
+	0.2,0.2,0.8,0.2
+},
+}
+beeps["v"]={
+fwav="sawtooth",
+frequency="C5",
+volume=0.5,
+duty=0.5,
+adsr={
+	0.7,
+	0.2,0.2,0.8,0.2
+},
+}
+beeps["b"]={
+fwav="toothsaw",
+frequency="C5",
+volume=0.5,
+duty=0.5,
+adsr={
+	0.7,
+	0.2,0.2,0.8,0.2
+},
+}
+beeps["n"]={
+fwav="square",
+frequency="C5",
+volume=0.5,
+duty=0.5,
+adsr={
+	0.7,
+	0.2,0.2,0.8,0.2
+},
+}
+
+beeps["m"]={
+fwav="whitenoise",
+frequency="C1",
+volume=1.0,
+duty=0.5,
+adsr={
+	0.7,
+	0.2,0.2,0.8,0.2
+},
+}
+beeps["k"]={
+fwav="square",
+frequency="C8",
+volume=1.0,
+duty=0.5,
+adsr={
+	0.7,
+	0.2,0.2,1.8,0.2
+},
+fm={
+	frequency=2,
+	fwav="sine",
+	duty=0.25,
+	ffreq=function(it)
+	local f1=note_to_freq("C3")
+	local f2=note_to_freq("C4")
+	local f3=note_to_freq("E3")
+	local t1=0
+	local f=function(m,t)
+--			return f2+t*t1
+		local t2=t*t1
+		if m<0 then return f2+((f1-f2)*-m)+t2 end
+		return f2+((f3-f2)*m)+t2
+--			return bitsynth.D3
+	end
+	return f
+	end,
+},
+}
+
+beeps["p"]={
+fwav="whitenoise",
+frequency="C5",
+volume=1.0,
+duty=0.5,
+adsr={
+	1.0,
+	0.0,0.0,0.0,0.5
+},
+fm={
+	frequency=16,
+	fwav="square",
+	duty=0.5,
+	ffreq=function(it)
+	local t1=-35*35
+	return function(m,t)
+		return note_to_freq("c5")+t*t1
+	end
+	end,
+},
+}
+
+
 oven.opts.fun="" -- back to menu on reset
 hardware,main=system.configurator({
 	mode="fun64", -- select the standard 320x240 screen using the swanky32 palette.
@@ -10,7 +409,6 @@ hardware,main=system.configurator({
 
 local wstr=require("wetgenes.string")
 
-beeps={}
 
 -- we will call this once in the update function
 setup=function()
@@ -22,388 +420,12 @@ setup=function()
     
     local scsfx=system.components.sfx
     local bitsynth=scsfx.bitsynth
-    
-    beeps["1"]={
-	fwav="sine",
-	duty=0.5,
-	adsr={
-	    1,
-	    0,0,0.4,0.1
-	},
-    }
-    
-    beeps["2"]={
-	fwav="triangle",
-	adsr={
-	    1,
-	    0,0,0.4,0.1
-	},
-    }
-
-    beeps["3"]={
-	fwav="sawtooth",
-	duty=0.125,
-	adsr={
-	    1,
-	    0,0,0.4,0.1
-	},
-    }
-
-    beeps["4"]={
-	fwav="toothsaw",
-	duty=0.125,
-	adsr={
-	    1,
-	    0,0,0.4,0.1
-	},
-    }
-
-    beeps["5"]={
-	fwav="square",
-	duty=0.5,
-	adsr={
-	    1,
-	    0,0,0.4,0.1
-	},
-    }
-
-    beeps["6"]={
-	fwav="whitenoise",
-	frequency="C2",
-	adsr={
-	    1,
-	    0,0,0.4,0.1
-	},
-    }
-    beeps["7"]={
-	fwav="whitenoise",
-	frequency="C4",
-	adsr={
-	    1,
-	    0,0,0.4,0.1
-	},
-    }
-    beeps["8"]={
-	fwav="whitenoise",
-	frequency="C6",
-	adsr={
-	    1,
-	    0,0,0.4,0.1
-	},
-    }
-
-    beeps["0"]={
-	fwav=function(t)
-	    local t2=t/8
-	    local n=math.sin( (t%1) * math.pi*2  ) * math.sin( ((t2)%1) * math.pi*2  )
-	    return n
-	end,
-	adsr={
-	    1,
-	    0,0,0.4,0.1
-	},
-    }
-
-    beeps["q"]={
-	fwav="sine",
-	adsr={
-	    0.5,
-	    0.0, 0.0, 2.0, 0.1
-	},
-	fm={
-	    frequency=8,
---	    fwav="toothsaw",
-	    fwav="sawtooth",
---	    fwav="square",
-	    ffreq=function(it)
-		local f1=bitsynth.note2freq("C4")
-		local f2=bitsynth.note2freq("D4")
-		local f3=bitsynth.note2freq("E4")
-		local t1=0
-		local f=function(m,t)
-			if m<0 then return f2+((f1-f2)*-m)+t*t1 end
-			return f2+((f3-f2)*m)+t*t1
-		end
-		return f
-	    end,
-	}
-    }
-
-    beeps["w"]={
-	fwav="sine",
-	adsr={
-	    1,
-	    0,0,1,0.1
-	},
-	fm={
-	    frequency=1024,
-	    fwav="sine",
-	    ffreq=function(it)
-		local f1=bitsynth.note2freq("C3")
-		local f2=bitsynth.note2freq("D3")
-		local f3=bitsynth.note2freq("E3")
-		local t1=64
-		local f=function(m,t)
-			if m<0 then return f2+((f1-f2)*-m)+bitsynth.fwav.square(t*4)*t1 end
-			return f2+((f3-f2)*m)+bitsynth.fwav.square(t*4)*t1
-		end
-		return f
-	    end,
-	}
-    }
-
-    beeps["e"]={
-	fwav="sine",
-	adsr={
-	    1,
-	    0,0,0.4,0.1
-	},
-	fm={
-	    frequency=16,
-	    fwav="sawtooth",
-	    ffreq=function(it)
-		local f1=bitsynth.note2freq("C3")
-		local f2=bitsynth.note2freq("C4")
-		local f3=bitsynth.note2freq("C5")
-		local t1=0
-		local f=function(m,t)
-			if m<0 then return f2+((f1-f2)*-m)+t*t1 end
-			return f2+((f3-f2)*m)+t*t1
-		end
-		return f
-	    end,
-	}
-    }
-
-    beeps["r"]={
-	fwav="sine",
-	adsr={
-	    1,
-	    0,0,0.4,0.1
-	},
-	fm={
-	    frequency=16,
-	    fwav="triangle",
-	    ffreq=function(it)
-		local f1=bitsynth.note2freq("C3")
-		local f2=bitsynth.note2freq("C4")
-		local f3=bitsynth.note2freq("C5")
-		local t1=0
-		local f=function(m,t)
-			if m<0 then return f2+((f1-f2)*-m)+t*t1 end
-			return f2+((f3-f2)*m)+t*t1
-		end
-		return f
-	    end,
-	}
-    }
-
-    beeps["t"]={
-	fwav="sawtooth",
-	adsr={
-	    1,
-	    0,0,0.4,0.5
-	},
-	fm={
-	    frequency=128,
-	    fwav="square",
-	    ffreq=function(it)
-		local f1=bitsynth.note2freq("C3")
-		local f2=bitsynth.note2freq("C4")
-		local f3=bitsynth.note2freq("C5")
-		local t1=0
-		local f=function(m,t)
-			if m<0 then return f2+((f1-f2)*-m)+t*t1 end
-			return f2+((f3-f2)*m)+t*t1
-		end
-		return f
-	    end,
-	}
-    }
-
-    beeps["a"]={
-	fwav="sawtooth",
-	adsr={
-	    0.75,
-	    0.0,0.1,0.4,0.3
-	},
-	fm={
-	    frequency=16,
-	    fwav="sine",
-	    duty=0.5,
-	    ffreq=function(it)
-		local f1=bitsynth.C3
-		local f2=bitsynth.D3
-		local f3=bitsynth.E3
-		local t1=0
-		local f=function(m,t)
---			return f2+t*t1
-			local t2=math.pow(t+2,4)*t1
-			if m<0 then return f2+((f1-f2)*-m)+t2 end
-			return f2+((f3-f2)*m)+t2
-		end
-		return f
-	    end,
-	},
-	fread=function(it)
-	    return function(t)
-		it.fm_gwav.set_frequency(2*(t+1))
-	    end
-	end
-    }
-
-    beeps["z"]={
-	fwav="triangle",
-	frequency="D4",
-	duty=0.25,
-	adsr={
-	    0.7,
-	    0.2,0.2,0.8,0.2
-	},
---[[
-	fm={
-	    frequency=16,
-	    fwav="sine",
-	    duty=0.5,
-	    ffreq=function(it)
-		local f1=bitsynth.C3
-		local f2=bitsynth.D3
-		local f3=bitsynth.E3
-		local t1=0
-		local f=function(m,t)
---			return f2+t*t1
---			local t2=math.pow(t+2,4)*t1
---			if m<0 then return f2+((f1-f2)*-m)+t2 end
---			return f2+((f3-f2)*m)+t2
-			return bitsynth.D3
-		end
-		return f
-	    end,
-	},
-]]
---	fread=function(it)
---	    return function(t)
---		it.fm_gwav.set_frequency(2*(t+1))
---	    end
---	end
-    }
-    
-
-    beeps["x"]={
-	fwav="sine",
-	frequency="C5",
-	volume=1.0,
-	duty=0.5,
-	adsr={
-	    0.7,
-	    0.2,0.2,0.8,0.2
-	},
-    }
-    beeps["c"]={
-	fwav="triangle",
-	frequency="C5",
-	volume=1.0,
-	duty=0.5,
-	adsr={
-	    0.7,
-	    0.2,0.2,0.8,0.2
-	},
-    }
-    beeps["v"]={
-	fwav="sawtooth",
-	frequency="C5",
-	volume=0.5,
-	duty=0.5,
-	adsr={
-	    0.7,
-	    0.2,0.2,0.8,0.2
-	},
-    }
-    beeps["b"]={
-	fwav="toothsaw",
-	frequency="C5",
-	volume=0.5,
-	duty=0.5,
-	adsr={
-	    0.7,
-	    0.2,0.2,0.8,0.2
-	},
-    }
-    beeps["n"]={
-	fwav="square",
-	frequency="C5",
-	volume=0.5,
-	duty=0.5,
-	adsr={
-	    0.7,
-	    0.2,0.2,0.8,0.2
-	},
-    }
-
-    beeps["m"]={
-	fwav="whitenoise",
-	frequency="C1",
-	volume=1.0,
-	duty=0.5,
-	adsr={
-	    0.7,
-	    0.2,0.2,0.8,0.2
-	},
-    }
-    beeps["k"]={
-	fwav="square",
-	frequency="C8",
-	volume=1.0,
-	duty=0.5,
-	adsr={
-	    0.7,
-	    0.2,0.2,1.8,0.2
-	},
-	fm={
-	    frequency=2,
-	    fwav="sine",
-	    duty=0.25,
-	    ffreq=function(it)
-		local f1=bitsynth.C3
-		local f2=bitsynth.C4
-		local f3=bitsynth.E3
-		local t1=0
-		local f=function(m,t)
---			return f2+t*t1
-			local t2=t*t1
-			if m<0 then return f2+((f1-f2)*-m)+t2 end
-			return f2+((f3-f2)*m)+t2
---			return bitsynth.D3
-		end
-		return f
-	    end,
-	},
-    }
-
-    beeps["p"]={
-	fwav="whitenoise",
-	frequency="C5",
-	volume=1.0,
-	duty=0.5,
-	adsr={
-	    1.0,
-	    0.0,0.0,0.0,0.5
-	},
-	fm={
-	    frequency=16,
-	    fwav="square",
-	    duty=0.5,
-	    ffreq=function(it)
-		local t1=-35*35
-		return function(m,t)
-		    return bitsynth.c5+t*t1
-		end
-	    end,
-	},
-    }
 
 
-    for n,v in pairs(beeps) do v.name=n scsfx.render(v) end
+    for n,v in pairs(beeps) do 
+		v.name=n
+		scsfx.render(v)
+    end
 
     print("Setup complete!")
 
