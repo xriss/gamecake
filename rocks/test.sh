a5464191dcec57aa91d77faa23fef2edd49f9227
kriss <kriss@xixs.com>
2026-08-18 20:26:50 +0100

    paths

--- rocks/test.sh
+++ rocks/test.sh
@@ -1,6 +1,5 @@
 
-export ROCK_ROOT=`pwd`
-
+ROCK_ROOT="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 source <(luarocks path)
 
@@ -10,7 +9,7 @@ cd ../test
 # this is needed to get all the command line args parsed and into _G.arg
 # this hack works on luajit and lua
 # gamecake module can now pretend it is a lua command
-LUA_PATH="$LUA_PATH;lua/?.lua" luajit ${ROCK_ROOT}/gamecake-loader.lua -- lua/unit.lua
+LUA_PATH="$LUA_PATH;lua/?.lua" luajit "$ROCK_ROOT/gamecake/src/lua_gamecake/code/gamecake.lua" -- lua/unit.lua
 
 
 # restore files we just broke
